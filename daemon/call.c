48018e48d04897dc33db36d286b1b84fec98ee45
Richard Fuchs <rfuchs@sipwise.com>
2013-03-15 10:33:15 -0400

    remove obsolete comment

--- daemon/call.c
+++ daemon/call.c
@@ -2256,7 +2256,7 @@ static void call_ng_process_flags(struct sdp_ng_flags *out, GQueue *streams, ben
 	if ((list = bencode_dictionary_get_expect(input, "flags", BENCODE_LIST))) {
 		for (it = list->child; it; it = it->sibling) {
 			if (!bencode_strcmp(it, "trust-address"))
-				out->trust_address = 1; /* XXX support this somehow */
+				out->trust_address = 1;
 			else if (!bencode_strcmp(it, "symmetric"))
 				out->symmetric = 1;
 			else if (!bencode_strcmp(it, "asymmetric"))
