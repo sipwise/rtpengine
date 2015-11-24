c969ab9f60a73e5815ca915ae59e69d818860aba
smititelu <stefan.mititelu@1and1.ro>
2015-11-24 13:10:14 +0100

    Change maxsessions feature behaviour
    *  0 will allow 0 sessions (e.g. can be used for draining rtpengine)
    * -1 will disable the feature and will be treated the same way as if
    MAX_SESSIONS variable has not been set via configuration file
    (or has been set to -1 in configuration file)
    * < -1 will not be taken into consideration
    * add check for minint range also

--- daemon/str.h
+++ daemon/str.h
@@ -248,6 +248,7 @@ INLINE int str_to_i(str *s, int def) {
 	char c, *ep;
 	long ret;
 	int maxint = 0x7FFFFFFF;
+	int minint = 0x80000000;
 	if (s->len <= 0)
 		return def;
 	c = s->s[s->len];
@@ -257,6 +258,8 @@ INLINE int str_to_i(str *s, int def) {
 	if (ep == s->s)
 		return def;
 	if (ret > maxint)
+		return def;
+	if (ret < minint)
 		return def;
 	return ret;
 }
