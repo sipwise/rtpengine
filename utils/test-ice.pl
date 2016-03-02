ced11f1223c94708d46b5dc299393f51db167658
Richard Fuchs <rfuchs@sipwise.com>
2016-03-02 15:20:15 -0500

    support sending rtp over ice negotiated peer WIP
    Change-Id: I1094913a54fbaf44d3ecf7bc38cc14113c873df8

--- utils/test-ice.pl
+++ utils/test-ice.pl
@@ -6,10 +6,11 @@ use Rtpengine::Test;
 
 my $r = Rtpengine::Test->new();
 my $a = $r->client(ice => 1);
-my $b = $r->client();
+my $b = $r->client(domain => &Socket::AF_INET);
 
 $r->timer_once(3, sub { $b->answer($a) });
+$r->timer_once(5, sub { $a->start_rtp(); $b->start_rtp(); });
 
-$a->offer($b, ICE => 'remove');
+$a->offer($b, ICE => 'remove', 'address-family' => 'IP4');
 
 $r->run();
