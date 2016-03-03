3f5f7c89eaf85bceea3401c3779acaa7bf464969
Richard Fuchs <rfuchs@sipwise.com>
2016-03-03 14:55:02 -0500

    support checking of received rtp data
    Change-Id: I5298ef0d581dc051042c981583badf6a7c7a4f54

--- utils/test-ice.pl
+++ utils/test-ice.pl
@@ -6,7 +6,7 @@ use Rtpengine::Test;
 
 my $r = Rtpengine::Test->new();
 my $a = $r->client(ice => 1);
-my $b = $r->client(domain => &Socket::AF_INET);
+my $b = $r->client(sockdomain => &Socket::AF_INET);
 
 $r->timer_once(3, sub { $b->answer($a) });
 $r->timer_once(5, sub { $a->start_rtp(); $b->start_rtp(); });
