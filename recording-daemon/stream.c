6475410c73e2ba7afbf8f7e43dcf49c920f4c7b4
Richard Fuchs <rfuchs@sipwise.com>
2016-11-17 15:43:43 -0500

    TT#5566 split RTP parsing functions into "lib" directory
    Change-Id: Icdc97a9dc849bba6ba6add12d0bdd17f8b7712cd

--- recording-daemon/stream.c
+++ recording-daemon/stream.c
@@ -5,10 +5,15 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <libavformat/avformat.h>
+#include <netinet/ip.h>
+#include <netinet/ip6.h>
+#include <netinet/udp.h>
 #include "metafile.h"
 #include "epoll.h"
 #include "log.h"
 #include "main.h"
+#include "rtplib.h"
+#include "str.h"
 
 
 // stream is locked
@@ -25,8 +30,18 @@ void stream_free(stream_t *stream) {
 }
 
 
+static void packet_free(packet_t *packet) {
+	if (!packet)
+		return;
+	free(packet->buffer);
+	g_slice_free1(sizeof(*packet), packet);
+}
+
+
 static void stream_handler(handler_t *handler) {
 	stream_t *stream = handler->ptr;
+	char *buf = NULL;
+	packet_t *packet = NULL;
 
 	//dbg("poll event for %s", stream->name);
 
@@ -35,8 +50,9 @@ static void stream_handler(handler_t *handler) {
 	if (stream->fd == -1)
 		goto out;
 
-	char buf[65535];
-	int ret = read(stream->fd, buf, sizeof(buf));
+	static const int maxbuflen = 65535;
+	buf = malloc(maxbuflen);
+	int ret = read(stream->fd, buf, maxbuflen);
 	if (ret == 0) {
 		ilog(LOG_INFO, "EOF on stream %s", stream->name);
 		stream_close(stream);
@@ -48,8 +64,41 @@ static void stream_handler(handler_t *handler) {
 		goto out;
 	}
 
+	// got a packet
+	packet = g_slice_alloc0(sizeof(*packet));
+	packet->buffer = buf;
+	buf = NULL;
+
+	// XXX more checking here, move to separate file?
+	str bufstr;
+	str_init_len(&bufstr, packet->buffer, ret);
+	packet->ip = (void *) bufstr.s;
+	if (packet->ip->version == 4) {
+		if (str_shift(&bufstr, packet->ip->ihl << 2))
+			goto err;
+	}
+	else {
+		packet->ip = NULL;
+		packet->ip6 = (void *) bufstr.s;
+		if (str_shift(&bufstr, sizeof(*packet->ip6)))
+			goto err;
+	}
+
+	packet->udp = (void *) bufstr.s;
+	str_shift(&bufstr, sizeof(*packet->udp));
+
+	if (rtp_payload(&packet->rtp, &packet->payload, &bufstr))
+		goto err;
+
+	dbg("packet parsed successfully");
+	goto out;
+
+err:
+	ilog(LOG_WARN, "Failed to parse packet headers");
 out:
 	pthread_mutex_unlock(&stream->lock);
+	free(buf);
+	packet_free(packet);
 }
 
 
@@ -89,14 +138,14 @@ void stream_open(metafile_t *mf, unsigned long id, char *name) {
 		return;
 	}
 
-	stream->avinf = av_find_input_format("rtp");
-	ilog(LOG_DEBUG, "avinf %p", stream->avinf);
+	//stream->avinf = av_find_input_format("rtp");
+	//ilog(LOG_DEBUG, "avinf %p", stream->avinf);
 
-	stream->avfctx = avformat_alloc_context();
-	unsigned char *buf = av_malloc(1024); // ?
-	stream->avfctx->pb = avio_alloc_context(buf, 1024, 1, NULL, NULL, NULL, NULL);
-	int ret = avformat_open_input(&stream->avfctx, "", stream->avinf, NULL);
-	ilog(LOG_DEBUG, "ret %i avfctx %p", ret, stream->avfctx);
+	//stream->avfctx = avformat_alloc_context();
+	//unsigned char *buf = av_malloc(1024); // ?
+	//stream->avfctx->pb = avio_alloc_context(buf, 1024, 1, NULL, NULL, NULL, NULL);
+	//int ret = avformat_open_input(&stream->avfctx, "", stream->avinf, NULL);
+	//ilog(LOG_DEBUG, "ret %i avfctx %p", ret, stream->avfctx);
 
 	// add to epoll
 	stream->handler.ptr = stream;
