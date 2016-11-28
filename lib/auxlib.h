dc4c028dfa8ff345e81cee0e72779cfb5af6e733
Richard Fuchs <rfuchs@sipwise.com>
2016-11-28 11:32:53 -0500

    TT#5566 add support for config file handling
    Change-Id: I92dd61c100319469c6ff4764c90bba5c3ab367a2

--- lib/auxlib.h
+++ lib/auxlib.h
@@ -1,9 +1,12 @@
 #ifndef _AUXLIB_H_
 #define _AUXLIB_H_
 
+#include <glib.h>
 
 void daemonize(void);
 void wpidfile(const char *pidfile);
+const char *config_load(int *argc, char ***argv, GOptionEntry *entries, const char *description,
+		char **filename_ptr, const char *default_config, char **section_ptr);
 
 
 #endif
