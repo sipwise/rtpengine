5038662d0ab042c52a38a58ee1aa6dadfb9d6b5d
Richard Fuchs <rfuchs@sipwise.com>
2016-11-28 11:32:53 -0500

    TT#5566 add support for config file handling
    closes #252
    
    Change-Id: I92dd61c100319469c6ff4764c90bba5c3ab367a2

--- lib/auxlib.c
+++ lib/auxlib.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <glib.h>
+#include <stdlib.h>
 #include "loglib.h"
 
 void daemonize(void) {
@@ -25,4 +27,71 @@ void wpidfile(const char *pidfile) {
 		fprintf(fp, "%u\n", getpid());
 		fclose(fp);
 	}
+}
+
+#define CONF_OPTION_GLUE(get_func, data_type, ...) 							\
+	{												\
+		data_type *varptr = e->arg_data;							\
+		data_type var = g_key_file_get_ ## get_func(kf, *section_ptr, e->long_name,		\
+			##__VA_ARGS__, &er);								\
+		if (er && g_error_matches(er, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_KEY_NOT_FOUND)) {	\
+			er = NULL;									\
+			break;										\
+		}											\
+		if (er)											\
+			return er->message;								\
+		*varptr = var;										\
+		break;											\
+	}
+
+const char *config_load(int *argc, char ***argv, GOptionEntry *entries, const char *description,
+		char **filename_ptr, const char *default_config, char **section_ptr)
+{
+	GOptionContext *c;
+	GError *er = NULL;
+	const char *config_file;
+	int fatal = 0;
+
+	c = g_option_context_new(description);
+	g_option_context_add_main_entries(c, entries, NULL);
+	if (!g_option_context_parse(c, argc, argv, &er))
+		return er->message;
+
+	// is there a config file to load?
+	config_file = default_config;
+	if (filename_ptr && *filename_ptr) {
+		config_file = *filename_ptr;
+		fatal = 1;
+	}
+
+	GKeyFile *kf = g_key_file_new();
+	if (!g_key_file_load_from_file(kf, config_file, G_KEY_FILE_NONE, &er)) {
+		if (!fatal && (g_error_matches(er, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_NOT_FOUND)
+					|| g_error_matches(er, G_FILE_ERROR, G_FILE_ERROR_NOENT)))
+			return NULL;
+		return er->message;
+	}
+
+	// iterate the options list and see if the config file defines any
+	for (GOptionEntry *e = entries; e->long_name; e++) {
+		switch (e->arg) {
+			case G_OPTION_ARG_NONE:
+				CONF_OPTION_GLUE(boolean, int);
+
+			case G_OPTION_ARG_INT:
+				CONF_OPTION_GLUE(integer, int);
+
+			case G_OPTION_ARG_STRING:
+			case G_OPTION_ARG_FILENAME:
+				CONF_OPTION_GLUE(string, char *);
+
+			case G_OPTION_ARG_STRING_ARRAY:
+				CONF_OPTION_GLUE(string_list, char **, NULL);
+
+			default:
+				abort();
+		}
+	}
+
+	return NULL;
 }
