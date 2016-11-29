326ac1bb525f75a6fb94bbd1416d4423586fb4a3
Richard Fuchs <rfuchs@sipwise.com>
2016-11-29 09:45:20 -0500

    TT#0 add additional audio codecs
    Change-Id: I4d9c62d16fbdb10c3c38fc5d7ec0e9dacf970702

--- recording-daemon/decoder.c
+++ recording-daemon/decoder.c
@@ -60,6 +60,16 @@ static const struct decoder_def_s decoders[] = {
 	DECODER_DEF(GSM, GSM),
 	DECODER_DEF(iLBC, ILBC),
 	DECODER_DEF_NAME(opus, OPUS, libopus),
+	DECODER_DEF_NAME(vorbis, VORBIS, libvorbis),
+	DECODER_DEF(ac3, AC3),
+	DECODER_DEF(eac3, EAC3),
+	DECODER_DEF(ATRAC3, ATRAC3),
+	DECODER_DEF(ATRAC-X, ATRAC3P),
+	DECODER_DEF(EVRC, EVRC),
+	DECODER_DEF(EVRC0, EVRC),
+	DECODER_DEF(EVRC1, EVRC),
+	DECODER_DEF(AMR, AMR_NB),
+	DECODER_DEF(AMR-WB, AMR_WB),
 };
 typedef struct decoder_def_s decoder_def_t;
 
