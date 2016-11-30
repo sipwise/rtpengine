f3e3104a39e01396253160f7add5532613e40dde
Richard Fuchs <rfuchs@sipwise.com>
2016-11-30 05:08:54 -0500

    TT#5566 support mp3 audio output
    Change-Id: Ife4001f5a9fcf8951c7c2b93d47fa2dcf2750a7b

--- recording-daemon/decoder.h
+++ recording-daemon/decoder.h
@@ -5,6 +5,8 @@
 #include "str.h"
 
 
+void output_init(const char *format);
+
 decoder_t *decoder_new(const char *payload_str);
 int decoder_input(decoder_t *, const str *, unsigned long ts, output_t *);
 void decoder_close(decoder_t *);
