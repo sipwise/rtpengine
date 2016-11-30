75f19ae6367dbf4ca0c9e930dd33fde5d29e4754
Richard Fuchs <rfuchs@sipwise.com>
2016-11-30 05:08:54 -0500

    TT#0 test
    Change-Id: Ife4001f5a9fcf8951c7c2b93d47fa2dcf2750a7b

--- recording-daemon/decoder.c
+++ recording-daemon/decoder.c
@@ -91,6 +91,17 @@ static const decoder_def_t *decoder_find(const str *name) {
 }
 
 
+static void print_supported_sample_rates(AVCodec *codec) {
+	if (!codec->supported_samplerates) {
+		dbg("codec %s has no supported sample rates", codec->name);
+		return;
+	}
+
+	for (const int *i = codec->supported_samplerates; *i; i++)
+		dbg("supported sample rate for %s: %i", codec->name, *i);
+}
+
+
 decoder_t *decoder_new(const char *payload_str) {
 	str name;
 	char *slash = strchr(payload_str, '/');
@@ -125,6 +136,7 @@ decoder_t *decoder_new(const char *payload_str) {
 		codec = avcodec_find_decoder_by_name(def->avcodec_name);
 	if (!codec)
 		codec = avcodec_find_decoder(def->avcodec_id);
+	print_supported_sample_rates(codec);
 
 	ret->avcctx = avcodec_alloc_context3(codec);
 	if (!ret->avcctx)
@@ -260,6 +272,7 @@ format_mismatch:
 		goto err;
 
 	AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_PCM_S16LE);
+	print_supported_sample_rates(codec);
 	// XXX error handling
 	output->avst = avformat_new_stream(output->fmtctx, codec);
 	if (!output->avst)
