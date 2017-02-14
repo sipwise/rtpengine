6f21333044457c8b8bad4515a175bc63cc6892ea
Richard Fuchs <rfuchs@sipwise.com>
2017-02-14 14:25:14 -0500

    TT#10156 write recordings metadata into mysql database
    Change-Id: If50b0528520fe816ed63494dc6c6e707fe9eca2c

--- recording-daemon/main.h
+++ recording-daemon/main.h
@@ -8,6 +8,11 @@ extern const char *spool_dir;
 extern const char *output_dir;
 extern int output_mixed;
 extern int output_single;
+extern const char *c_mysql_host,
+      *c_mysql_user,
+      *c_mysql_pass,
+      *c_mysql_db;
+extern int c_mysql_port;
 
 extern volatile int shutdown_flag;
 
