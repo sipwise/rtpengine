aef463f1bb927542f40a5a9ea71feada6d44bfa5
Richard Fuchs <rfuchs@sipwise.com>
2017-03-30 09:12:08 -0400

    handle missing av_frame_get_channels from older versions
    fixes #341
    
    Change-Id: I881b63021c9aba0006bb35bd38b027fa3b0152f9

--- recording-daemon/resample.c
+++ recording-daemon/resample.c
@@ -37,9 +37,11 @@ resample:
 		if (!resample->avresample)
 			goto err;
 
+#if LIBAVUTIL_VERSION_INT >= AV_VERSION_INT(55, 0, 0)
 		if (!frame->channel_layout)
 			frame->channel_layout = av_get_default_channel_layout(
 				av_frame_get_channels(frame));
+#endif
 
 		err = "failed to set resample option";
 		if ((errcode = av_opt_set_int(resample->avresample, "in_channel_layout",
