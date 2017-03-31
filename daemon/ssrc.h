ff575a968ee27d9e24cf289491cdb98f2ad18f73
Richard Fuchs <rfuchs@sipwise.com>
2017-03-31 12:37:05 -0400

    TT#12800 store and process RTCP SR and RR for statistics
    Change-Id: Ia78c37ae9d24df6783b664da6d395263f9f39e91

--- daemon/ssrc.h
+++ daemon/ssrc.h
@@ -10,6 +10,11 @@
 
 
 
+struct call;
+struct timeval;
+
+
+
 struct ssrc_hash {
 	GHashTable *ht;
 	rwlock_t lock;
@@ -20,14 +25,45 @@ struct ssrc_ctx {
 	// XXX move entire crypto context in here?
 };
 struct ssrc_entry {
-	// XXX lock this?
+	mutex_t lock;
 	u_int32_t ssrc;
 	struct ssrc_ctx input_ctx,
 			output_ctx;
+	GQueue sender_reports;
 };
 enum ssrc_dir {
 	SSRC_DIR_INPUT  = G_STRUCT_OFFSET(struct ssrc_entry, input_ctx),
 	SSRC_DIR_OUTPUT = G_STRUCT_OFFSET(struct ssrc_entry, output_ctx),
+};
+
+struct ssrc_sender_report {
+	u_int32_t ssrc;
+	u_int32_t ntp_msw;
+	u_int32_t ntp_lsw;
+	u_int32_t timestamp;
+	u_int32_t packet_count;
+	u_int32_t octet_count;
+	double ntp_ts;
+};
+struct ssrc_sender_report_item {
+	struct timeval received;
+	u_int32_t ntp_middle_bits; // to match up with rr->lsr
+	struct ssrc_sender_report report;
+};
+
+struct ssrc_receiver_report {
+	u_int32_t from;
+	u_int32_t ssrc;
+	unsigned char fraction_lost;
+	u_int32_t packets_lost;
+	u_int32_t high_seq_received;
+	u_int32_t jitter;
+	u_int32_t lsr;
+	u_int32_t dlsr;
+};
+struct ssrc_receiver_report_item {
+	struct timeval received;
+	struct ssrc_receiver_report report;
 };
 
 
@@ -41,6 +77,11 @@ struct ssrc_entry *get_ssrc(u_int32_t, struct ssrc_hash * /* , int *created */);
 //void add_ssrc_entry(struct ssrc_entry *, struct ssrc_hash *); // XXX static
 //struct ssrc_entry *create_ssrc_entry(u_int32_t);
 struct ssrc_ctx *get_ssrc_ctx(u_int32_t, struct ssrc_hash *, enum ssrc_dir); // creates new entry if not found
+
+
+void ssrc_sender_report(struct call *, const struct ssrc_sender_report *, const struct timeval *);
+void ssrc_receiver_report(struct call *, const struct ssrc_receiver_report *,
+		const struct timeval *);
 
 
 
