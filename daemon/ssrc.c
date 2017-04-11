fc648681dc120f707135dcbc379339ecac05650c
Richard Fuchs <rfuchs@sipwise.com>
2017-04-11 16:07:02 -0400

    TT#12800 add MOS statistics to final log output
    also converts call->created to a timeval
    
    Change-Id: Idb71ada22a2d04637670b7515e54f2994f8d4c93

--- daemon/ssrc.c
+++ daemon/ssrc.c
@@ -41,7 +41,7 @@ static void free_ssrc_entry(void *p) {
 static void mos_calc(struct ssrc_stats_block *ssb) {
 	// as per https://www.pingman.com/kb/article/how-is-mos-calculated-in-pingplotter-pro-50.html
 	int eff_rtt = ssb->rtt / 1000 + ssb->jitter * 2 + 10;
-	double r;
+	double r; // XXX can this be done with int math?
 	if (eff_rtt < 160)
 		r = 93.2 - eff_rtt / 40.0;
 	else
@@ -211,8 +211,26 @@ found:
 
 	// got a new stats block, add it to reporting ssrc
 	e = get_ssrc(rr->from, c->ssrc_hash);
+
 	mutex_lock(&e->lock);
+
+	// discard stats block if last has been received less than a second ago
+	if (G_LIKELY(e->stats_blocks.length > 0)) {
+		struct ssrc_stats_block *last_ssb = g_queue_peek_tail(&e->stats_blocks);
+		if (G_UNLIKELY(timeval_diff(tv, &last_ssb->reported) < 1000000)) {
+			free_stats_block(ssb);
+			goto out;
+		}
+	}
+
 	g_queue_push_tail(&e->stats_blocks, ssb);
+
+	if (G_UNLIKELY(!e->lowest_mos) || ssb->mos < e->lowest_mos->mos)
+		e->lowest_mos = ssb;
+	if (G_UNLIKELY(!e->highest_mos) || ssb->mos > e->highest_mos->mos)
+		e->highest_mos = ssb;
+	e->mos_sum += ssb->mos;
+
 	goto out;
 
 out:
