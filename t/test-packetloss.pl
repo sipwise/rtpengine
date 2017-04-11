021565896db371cd38352a61be63a3796de23bcc
Richard Fuchs <rfuchs@sipwise.com>
2017-04-11 10:00:06 -0400

    TT#12800 add test script to simulate RTP packet loss
    Change-Id: I931d2b0ee16167bf48900aee806d99a12e4c601e

---
+++ t/test-packetloss.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use NGCP::Rtpengine::Test;
+use IO::Socket;
+
+my $r = NGCP::Rtpengine::Test->new();
+my ($a, $b) = $r->client_pair(
+	{
+		sockdomain => &Socket::AF_INET,
+		packetloss => 5,
+	},
+	{
+		sockdomain => &Socket::AF_INET,
+		packetloss => 10,
+	}
+);
+
+$r->timer_once(1, sub {
+		$b->answer($a, ICE => 'remove');
+		$a->start_rtp();
+		$a->start_rtcp();
+	});
+$r->timer_once(60, sub { $r->stop(); });
+
+$a->offer($b, ICE => 'remove');
+$b->start_rtp();
+$b->start_rtcp();
+
+$r->run();
+
+$a->teardown();
