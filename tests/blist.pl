70606f84f9f690d18fa941cb6ec4d8db587ec373
Guillem Jover <gjover@sipwise.com>
2017-11-08 17:17:37 +0100

    TT#24097 Do not use magic variable names
    These variables have magic meanings, we should avoid their usage to not
    confuse readers nor trigger side-effects.
    
    Change-Id: Ieb759f74ccde6ba6466f5c02743e3f881b5d53b7

--- tests/blist.pl
+++ tests/blist.pl
@@ -13,21 +13,21 @@ my $len = length(pack($format, (0) x 100));
 open(my $fh, "<", "/proc/rtpengine/$t/blist") or die;
 my $buf;
 while (sysread($fh, $buf, $len)) {
-	my @b = unpack($format, $buf);
+	my @buf = unpack($format, $buf);
 	for (2,6,10) {
-		if ($b[$_] == AF_INET) {
-			$b[$_ + 1] = inet_ntoa($b[$_ + 1]);
+		if ($buf[$_] == AF_INET) {
+			$buf[$_ + 1] = inet_ntoa($buf[$_ + 1]);
 		}
-		elsif ($b[$_] == AF_INET6) {
-			$b[$_ + 1] = inet_ntop(AF_INET6, $b[$_ + 1]);
+		elsif ($buf[$_] == AF_INET6) {
+			$buf[$_ + 1] = inet_ntop(AF_INET6, $buf[$_ + 1]);
 		}
-		elsif ($b[$_] == 0) {
-			$b[$_ + 1] = '---';
+		elsif ($buf[$_] == 0) {
+			$buf[$_ + 1] = '---';
 		}
 	}
 	for (18, 20, 22) {
-		$b[$_] += $b[$_ + 1] * 2**32;
+		$buf[$_] += $buf[$_ + 1] * 2**32;
 	}
-	printf("%5u %15s:%-5u -> %15s:%-5u (-> %15s:%-5u) [%u] [%llu %llu %llu]\n", @b[0,3,4,7,8,11,12,14,18,20,22]);
+	printf("%5u %15s:%-5u -> %15s:%-5u (-> %15s:%-5u) [%u] [%llu %llu %llu]\n", @buf[0,3,4,7,8,11,12,14,18,20,22]);
 }
 close($fh);
