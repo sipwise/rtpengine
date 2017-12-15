4e008866fd105e6eb41c4e59bcf10d2dd2939136
Richard Fuchs <rfuchs@sipwise.com>
2017-12-15 11:22:27 -0500

    TT#26756 clean up CLI code using streambuf
    Change-Id: I0e6c264d5cbd569fb35fab3ad2b0a4f6413949b0

--- daemon/tcp_listener.c
+++ daemon/tcp_listener.c
@@ -100,7 +100,9 @@ static void streambuf_stream_free(void *p) {
 
 static void streambuf_stream_closed(int fd, void *p, uintptr_t u) {
 	struct streambuf_stream *s = p;
-	s->cb->closed_func(s);
+
+	if (s->cb->closed_func)
+		s->cb->closed_func(s);
 
 	struct streambuf_listener *l = s->listener;
 	mutex_lock(&l->lock);
@@ -113,7 +115,8 @@ static void streambuf_stream_closed(int fd, void *p, uintptr_t u) {
 
 static void streambuf_stream_timer(int fd, void *p, uintptr_t u) {
 	struct streambuf_stream *s = p;
-	s->cb->timer_func(s);
+	if (s->cb->timer_func)
+		s->cb->timer_func(s);
 }
 
 
