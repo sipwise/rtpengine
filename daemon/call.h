7051f9258d5cfaedb829011a7145ddbcb2b4883f
Richard Fuchs <rfuchs@sipwise.com>
2018-01-24 10:25:10 -0500

    TT#28163 abstractize SSRC hash
    Change-Id: I2f28854070ad5a7567870ef7585e67729154b0e3

--- daemon/call.h
+++ daemon/call.h
@@ -201,6 +201,7 @@ struct call_monologue;
 struct ice_agent;
 struct ssrc_hash;
 struct codec_handler;
+struct rtp_payload_type;
 
 
 typedef bencode_buffer_t call_buffer_t;
@@ -426,7 +427,7 @@ int call_stream_address46(char *o, struct packet_stream *ps, enum stream_address
 const struct transport_protocol *transport_protocol(const str *s);
 void add_total_calls_duration_in_interval(struct timeval *interval_tv);
 
-void __payload_type_free(void *p);
+void payload_type_free(struct rtp_payload_type *p);
 void __rtp_stats_update(GHashTable *dst, GHashTable *src);
 
 const struct rtp_payload_type *__rtp_stats_codec(struct call_media *m);
