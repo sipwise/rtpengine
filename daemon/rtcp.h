c09dafdc1e9f59e6b8619608052e36c2f9b73591
Richard Fuchs <rfuchs@sipwise.com>
2018-02-07 15:44:24 -0500

    TT#31403 map and replace RTCP SSRCs when transcoding
    Change-Id: I765f1972e4b4d10d48e10a9e14e451edd48ca836

--- daemon/rtcp.h
+++ daemon/rtcp.h
@@ -10,7 +10,17 @@
 struct crypto_context;
 struct rtcp_packet;
 struct ssrc_ctx;
+struct rtcp_handler;
 
+
+struct rtcp_parse_ctx {
+	struct call *call;
+	struct call_media *media;
+	const struct timeval *received;
+};
+
+
+extern struct rtcp_handler *rtcp_transcode_handler;
 
 
 int rtcp_avp2savp(str *, struct crypto_context *, struct ssrc_ctx *);
@@ -18,7 +28,7 @@ int rtcp_savp2avp(str *, struct crypto_context *, struct ssrc_ctx *);
 
 int rtcp_payload(struct rtcp_packet **out, str *p, const str *s);
 
-int rtcp_parse(GQueue *q, const str *, struct stream_fd *sfd, const endpoint_t *, const struct timeval *);
+int rtcp_parse(GQueue *q, struct media_packet *);
 void rtcp_list_free(GQueue *q);
 
 rtcp_filter_func rtcp_avpf2avp_filter;
