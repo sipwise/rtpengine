018c343a2f50941873d2bf5761899d340845d81a
Richard Fuchs <rfuchs@sipwise.com>
2018-03-08 11:38:24 -0500

    TT#34050 replace ffmpeg version test macro
    Change-Id: Ia7aa9d63e09a90afc2814f0edede480c06ecc53a

---
+++ lib/fix_frame_channel_layout-01.h
@@ -0,0 +1,9 @@
+#include <libavutil/frame.h>
+#include <libavutil/channel_layout.h>
+#include "compat.h"
+
+INLINE void fix_frame_channel_layout(AVFrame *frame) {
+	if (frame->channel_layout)
+		return;
+	frame->channel_layout = av_get_default_channel_layout(av_frame_get_channels(frame));
+}
