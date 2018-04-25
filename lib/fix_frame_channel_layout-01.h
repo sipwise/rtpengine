b1dc3cf423fa31763b130599505a83c768ead78b
Anthony Alba <ascanio.alba7@gmail.com>
2018-04-25 08:03:21 +0000

    ffmpeg: handle some API changes for release/4.0 master

--- lib/fix_frame_channel_layout-01.h
+++ lib/fix_frame_channel_layout-01.h
@@ -5,5 +5,9 @@
 INLINE void fix_frame_channel_layout(AVFrame *frame) {
 	if (frame->channel_layout)
 		return;
+#if LIBAVUTIL_VERSION_MAJOR >= 56
+	frame->channel_layout = av_get_default_channel_layout(frame->channels);
+#else
 	frame->channel_layout = av_get_default_channel_layout(av_frame_get_channels(frame));
+#endif
 }
