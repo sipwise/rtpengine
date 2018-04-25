10d9acda3f2f80f89d90fda378a5e427209760fb
Richard Fuchs <rfuchs@sipwise.com>
2018-04-25 09:17:09 -0400

    ffmpeg: handle some API changes for release/4.0 master
    closes #534
    
    Change-Id: I02ca8e2e3cbe6193705b34f6f2df09205b283022

--- lib/fix_frame_channel_layout-01.h
+++ lib/fix_frame_channel_layout-01.h
@@ -3,7 +3,16 @@
 #include "compat.h"
 
 INLINE void fix_frame_channel_layout(AVFrame *frame) {
-	if (frame->channel_layout)
+	if (frame->channel_layout) {
+#if LIBAVUTIL_VERSION_MAJOR < 56
+		if (!frame->channels)
+			frame->channels = av_frame_get_channels(frame);
+#endif
 		return;
+	}
+#if LIBAVUTIL_VERSION_MAJOR < 56
 	frame->channel_layout = av_get_default_channel_layout(av_frame_get_channels(frame));
+#else
+	frame->channel_layout = av_get_default_channel_layout(frame->channels);
+#endif
 }
--- lib/fix_frame_channel_layout-04.h
+++ lib/fix_frame_channel_layout-04.h
@@ -3,10 +3,7 @@
 #include "compat.h"
 
 INLINE void fix_frame_channel_layout(AVFrame *frame) {
-	if (frame->channel_layout) {
-		if (!frame->channels)
-			frame->channels = av_frame_get_channels(frame);
+	if (frame->channel_layout)
 		return;
-	}
 	frame->channel_layout = av_get_default_channel_layout(frame->channels);
 }
