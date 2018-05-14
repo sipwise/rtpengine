f405c617c134c8c3bb75393e3ad36b6e0b60b223
Richard Fuchs <rfuchs@sipwise.com>
2018-05-14 13:57:05 -0400

    TT#36301 transcoding unit test - WIP
    Change-Id: If48e5afe5a343b557183b6e87441f52424dd2c6a

--- t/log.h
+++ t/log.h
@@ -4,4 +4,11 @@
 #include "loglib.h"
 #define __ilog(prio, fmt, ...) fprintf(stderr, fmt "\n", ##__VA_ARGS__)
 
+INLINE void rtcplog(const char *x) {
+}
+INLINE void cdrlog(const char *x) {
+}
+extern int _log_facility_rtcp;
+extern int _log_facility_cdr;
+
 #endif
---
+++ t/log_funcs.h
@@ -0,0 +1,24 @@
+#ifndef _LOG_FUNCS_H_
+#define _LOG_FUNCS_H_
+
+#include "aux.h"
+#include "str.h"
+
+struct call;
+struct stream_fd;
+struct ice_agent;
+
+INLINE void log_info_clear() {
+}
+INLINE void log_info_call(struct call *c) {
+}
+INLINE void log_info_stream_fd(struct stream_fd *sfd) {
+}
+INLINE void log_info_str(const str *s) {
+}
+INLINE void log_info_c_string(const char *s) {
+}
+INLINE void log_info_ice_agent(struct ice_agent *ag) {
+}
+
+#endif
