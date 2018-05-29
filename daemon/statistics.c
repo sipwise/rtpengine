sipwise/rtpengine

commit db58aa80d0b1ca942cf290099058ae5f458da4ce

fix graphite interval calculation for call durations
closes #543

Change-Id: I60f88cd874051c44df21b0b20401d1603ba5832e

daemon/statistics.c
@@ -46,7 +46,12 @@ static void timeval_totalstats_interval_call_duration_add(struct totalstats *s,
 
 	/* in case graphite interval needs to be the previous one */
 	if (timercmp(&real_iv_start, call_stop, >)) {
-		struct timeval graph_dur = { .tv_sec = interval_dur_s, .tv_usec = 0LL };
+		// round up to nearest while interval_dur_s
+		long long d = timeval_diff(&real_iv_start, call_stop);
+		d += (interval_dur_s * 1000000) - 1;
+		d /= 1000000 * interval_dur_s;
+		d *= interval_dur_s;
+		struct timeval graph_dur = { .tv_sec = d, .tv_usec = 0LL };
 		timeval_subtract(&real_iv_start, interval_start, &graph_dur);
 	}
 

commit 23f0ee20dbabbf02a3930d3845f80d3d38935c9c

TT#37156 fix divide by zero error (regression from db58aa80)
Change-Id: Ifdc58c814d6a0c82d95ca86ab2fd4eb3a85486ae

daemon/statistics.c
@@ -45,7 +45,7 @@ static void timeval_totalstats_interval_call_duration_add(struct totalstats *s,
 	struct timeval *call_start_in_iv = call_start;
 
 	/* in case graphite interval needs to be the previous one */
-	if (timercmp(&real_iv_start, call_stop, >)) {
+	if (timercmp(&real_iv_start, call_stop, >) && interval_dur_s) {
 		// round up to nearest while interval_dur_s
 		long long d = timeval_diff(&real_iv_start, call_stop);
 		d += (interval_dur_s * 1000000) - 1;