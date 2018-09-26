b3b6ea8abbdbf43c0bad9876c5de906c4b807571
Richard Fuchs <rfuchs@sipwise.com>
2018-09-26 15:38:52 -0400

    TT#41900 save monologue label into recording DB
    closes #582
    
    Change-Id: Ibc894e82b61e4231c99f9618685a6232f7e50da7

--- recording-daemon/stream.h
+++ recording-daemon/stream.h
@@ -4,6 +4,7 @@
 #include "types.h"
 
 void stream_open(metafile_t *mf, unsigned long id, char *name);
+void stream_details(metafile_t *mf, unsigned long id, unsigned int tag);
 void stream_close(stream_t *stream);
 void stream_free(stream_t *stream);
 
