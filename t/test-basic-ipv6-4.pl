7382895f8b620f090f258b2536e51b58d6054711
Richard Fuchs <rfuchs@sipwise.com>
2018-12-31 15:30:06 -0500

    TT#49351 create preloaded library for system tests
    Change-Id: Ibd87e7c03637a0772dcf3b0f2ed0d8646aa2add9

---
+++ t/test-basic-ipv6-4.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use NGCP::Rtpengine::Test;
+use IO::Socket;
+
+my $r = NGCP::Rtpengine::Test->new();
+my ($a, $b) = $r->client_pair(
+	{sockdomain => &Socket::AF_INET6},
+	{sockdomain => &Socket::AF_INET}
+);
+
+$r->timer_once(3, sub {
+		$b->answer($a, ICE => 'remove', label => "callee");
+		$a->start_rtp();
+		$a->start_rtcp();
+	});
+$r->timer_once(10, sub { $r->stop(); });
+
+$a->offer($b, ICE => 'remove', label => "caller", 'address-family' => 'IP4');
+$b->start_rtp();
+$b->start_rtcp();
+
+$r->run();
+
+$a->teardown(dump => 1);
