665926e3140b46193f3c0be3a5971f8d5c9c17ae
Richard Fuchs <rfuchs@sipwise.com>
2019-01-23 12:13:19 -0500

    don't add rtpmap or mid attributes for deleted/silenced streams
    fixes #686
    
    Change-Id: Ie9a155072a8b6acb8edcf59a5dd164f78a503616
    (cherry picked from commit 415af36ae62d6fc9f50a5a797ef5681009d8aa0f)
    (cherry picked from commit 6989151ee7ca70366d57e9507cad47858684bd68)

--- daemon/sdp.c
+++ daemon/sdp.c
@@ -2063,6 +2063,9 @@ int sdp_replace(struct sdp_chopper *chop, GQueue *sessions, struct call_monologu
 
 			copy_up_to_end_of(chop, &sdp_media->s);
 
+			if (!sdp_media->port_num || !ps->selected_sfd)
+				goto next;
+
 			insert_codec_parameters(chop, call_media);
 
 			ps_rtcp = NULL;
@@ -2073,9 +2076,6 @@ int sdp_replace(struct sdp_chopper *chop, GQueue *sessions, struct call_monologu
 					goto error;
 				assert(j->data == ps_rtcp);
 			}
-
-			if (!sdp_media->port_num || !ps->selected_sfd)
-				goto next;
 
 			if (MEDIA_ARESET2(call_media, SEND, RECV))
 				chopper_append_c(chop, "a=sendrecv\r\n");
