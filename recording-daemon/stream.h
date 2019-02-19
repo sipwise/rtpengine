cb2dbd2a92e1fe81d1ab66aa34ce52849e4cc261
Richard Fuchs <rfuchs@sipwise.com>
2019-02-19 09:32:56 -0500

    TT#52651 add start/stop forwarding commands and party selection logic
    Change-Id: I8ef7e288d3a3e485bd2fa14e1a2407a0c8d94bac

--- recording-daemon/stream.h
+++ recording-daemon/stream.h
@@ -5,6 +5,7 @@
 
 void stream_open(metafile_t *mf, unsigned long id, char *name);
 void stream_details(metafile_t *mf, unsigned long id, unsigned int tag);
+void stream_forwarding_on(metafile_t *mf, unsigned long id, unsigned int on);
 void stream_close(stream_t *stream);
 void stream_free(stream_t *stream);
 
