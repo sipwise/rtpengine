eafe45259c4290fe6a19feb53e556e1e3e53b900
Richard Fuchs <rfuchs@sipwise.com>
2019-02-22 09:20:54 -0500

    TT#52651 generalise streambuf interface
    Change-Id: I7d5ab8ffe13e52d4dbb1901531cc13fcc173d60d

--- recording-daemon/poller.c
+++ recording-daemon/poller.c
@@ -1,10 +1,10 @@
 #include "poller.h"
 
-void poller_blocked(struct poller *p, int fd) {
+void poller_blocked(struct poller *p, void *fdp) {
 	p->blocked = 1;
 }
-int poller_isblocked(struct poller *p, int fd) {
+int poller_isblocked(struct poller *p, void *fdp) {
 	return p->blocked ? 1 : 0;
 }
-void poller_error(struct poller *p, int fd) {
+void poller_error(struct poller *p, void *fdp) {
 }
