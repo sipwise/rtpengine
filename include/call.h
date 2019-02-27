ab805567e228119f696dbe4cdd3cd0abe2261d4d
Richard Fuchs <rfuchs@sipwise.com>
2019-02-27 16:55:48 -0500

    TT#50652 infrastructure to support reading/playing of media files
    Change-Id: I8212f5c74bcb9557d41f80ef3a1595f53bafde8a

--- include/call.h
+++ include/call.h
@@ -198,6 +198,7 @@ struct ice_agent;
 struct ssrc_hash;
 struct codec_handler;
 struct rtp_payload_type;
+struct media_player;
 
 
 typedef bencode_buffer_t call_buffer_t;
@@ -361,6 +362,7 @@ struct call_monologue {
 	struct call_monologue	*active_dialogue;
 	GQueue			medias;
 	GHashTable		*media_ids;
+	struct media_player	*player;
 
 	int			block_dtmf:1;
 	int			block_media:1;
