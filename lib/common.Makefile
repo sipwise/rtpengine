sipwise/rtpengine

commit 1f10dc30d529fd1d796fb379654f4e3d905d749b

TT#49106 Move the usage information into a new man page
Change-Id: I837d65c624cf1cabad543236b0a4e36f57894bab

lib/common.Makefile
@@ -1,7 +1,7 @@
 include ../lib/lib.Makefile
 
 all:
-	$(MAKE) $(TARGET)
+	$(MAKE) $(TARGET) $(MANS)
 
 $(TARGET):	$(OBJS) .depend Makefile
 	$(CC) $(LDFLAGS) $(CFLAGS) -o $@ $(OBJS) $(LDLIBS)
@@ -31,6 +31,13 @@ $(DAEMONSRCS) $(HASHSRCS):	$(patsubst %,../daemon/%,$(DAEMONSRCS)) $(patsubst %,
 		rm -f "$@"
 		echo '/******** GENERATED FILE ********/' > "$@"
 		cat ../daemon/"$@" >> "$@"
+
+%.8: %.pod
+	pod2man \
+		--center="NGCP rtpengine" \
+		--date="$(RELEASE_DATE)" \
+		--release="$(RTPENGINE_VERSION)" \
+		$< $@
 
 resample.c:	fix_frame_channel_layout.h
 

daemon/.gitignore
@@ -1,4 +1,5 @@
 .depend
+*.8
 *.o
 rtpengine
 core

daemon/Makefile
@@ -130,6 +130,9 @@ LIBSRCS+=	codeclib.c resample.c
 endif
 OBJS=		$(SRCS:.c=.o) $(LIBSRCS:.c=.o)
 
+PODS=		rtpengine.pod
+MANS=		$(PODS:.pod=.8)
+
 include ../lib/common.Makefile
 
 include		.depend

debian/ngcp-rtpengine-daemon.manpages
@@ -1,0 +1,1 @@
+daemon/rtpengine.8

lib/lib.Makefile
@@ -5,9 +5,18 @@ ifeq ($(RTPENGINE_ROOT_DIR),)
 	RTPENGINE_ROOT_DIR=..
 endif
 
+HAVE_DPKG_PARSECHANGELOG?=$(shell which dpkg-parsechangelog 2>/dev/null)
+
+ifeq ($(RELEASE_DATE),)
+  ifneq ($(HAVE_DPKG_PARSECHANGELOG),)
+    RELEASE_DATE=$(shell date -d "@$$(dpkg-parsechangelog -l$(RTPENGINE_ROOT_DIR)/debian/changelog -STimestamp)" '+%F')
+  else
+    RELEASE_DATE=undefined
+  endif
+endif
+
 ifeq ($(RTPENGINE_VERSION),)
-  DPKG_PRSCHNGLG= $(shell which dpkg-parsechangelog 2>/dev/null)
-  ifneq ($(DPKG_PRSCHNGLG),)
+  ifneq ($(HAVE_DPKG_PARSECHANGELOG),)
     DPKG_PRSCHNGLG=$(shell dpkg-parsechangelog -l$(RTPENGINE_ROOT_DIR)/debian/changelog | awk '/^Version: / {print $$2}')
   endif
   GIT_BR_COMMIT=git-$(shell git rev-parse --abbrev-ref --symbolic-full-name HEAD 2> /dev/null)-$(shell git rev-parse --short HEAD 2> /dev/null)

commit 5e3a59a838270a9b369ebb16bc4e59c32aed9f13

TT#49106 Clean up generated man pages
Change-Id: I5fc089dff955a37403d170c81ba5d0d42eeb8415
Fixes: commit 1f10dc30d529fd1d796fb379654f4e3d905d749b

lib/common.Makefile
@@ -12,7 +12,7 @@ debug:
 dep:		.depend
 
 clean:
-	rm -f $(OBJS) $(TARGET) $(LIBSRCS) $(DAEMONSRCS) $(ADD_CLEAN) .depend core core.*
+	rm -f $(OBJS) $(TARGET) $(LIBSRCS) $(DAEMONSRCS) $(MANS) $(ADD_CLEAN) .depend core core.*
 	rm -f fix_frame_channel_layout.h fix_frame_channel_layout-test.[co] *.strhash.c $(HASHSRCS)
 
 .depend:	$(SRCS) $(LIBSRCS) $(DAEMONSRCS) Makefile