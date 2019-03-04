f8b69e91717071215c176cf014aa265f66103d72
Richard Fuchs <rfuchs@sipwise.com>
2019-03-04 14:57:09 -0500

    TT#50652 fix compilation for with_transcode=no
    Change-Id: Ie65827be7e27a6f77019a983a1ce131c11b694d3

--- daemon/call_interfaces.c
+++ daemon/call_interfaces.c
@@ -1739,6 +1739,7 @@ out:
 
 
 const char *call_play_media_ng(bencode_item_t *input, bencode_item_t *output) {
+#ifdef WITH_TRANSCODING
 	str callid, fromtag, file;
 	struct call *call;
 	struct call_monologue *monologue;
@@ -1783,6 +1784,9 @@ out:
 	rwlock_unlock_w(&call->master_lock);
 	obj_put(call);
 	return err;
+#else
+	return "unsupported";
+#endif
 }
 
 
