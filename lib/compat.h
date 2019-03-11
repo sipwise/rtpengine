13e5480ecf9d4e08c970581038763a43d72277e8
Richard Fuchs <rfuchs@sipwise.com>
2019-03-11 07:58:51 -0400

    normalise compat.h handling towards kamailio tree
    Change-Id: Ia22a603c11b216969f4c0a854af42600a99a2819

--- lib/compat.h
+++ lib/compat.h
@@ -7,4 +7,12 @@
 # define INLINE static inline __attribute__((always_inline))
 #endif
 
+
+#ifndef BENCODE_MALLOC
+#define BENCODE_MALLOC malloc
+#define BENCODE_FREE free
+#endif
+
+#include "str.h"
+
 #endif
