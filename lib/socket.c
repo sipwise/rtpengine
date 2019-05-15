af02779bc9aecb2db4e20d0015a28947685f8204
Richard Fuchs <rfuchs@sipwise.com>
2019-05-15 07:49:39 -0400

    increase buffer size for cmsg
    apparently needed for some kernel versions/configs?
    
    fixes #770
    
    Change-Id: Ib39854df47b8c9d5590590abd24d438b504c1ffd

--- lib/socket.c
+++ lib/socket.c
@@ -295,7 +295,7 @@ static ssize_t __ip_recvfrom_ts(socket_t *s, void *buf, size_t len, endpoint_t *
 	struct sockaddr_storage sin;
 	struct msghdr msg;
 	struct iovec iov;
-	char ctrl[32];
+	char ctrl[64];
 	struct cmsghdr *cm;
 
 	ZERO(msg);
