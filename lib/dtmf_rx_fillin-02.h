04125bf362b205063e7cffe7973bbd5f245c8ef2
Richard Fuchs <rfuchs@sipwise.com>
2019-06-03 11:35:57 -0400

    TT#59703 add build test for dtmf_rx_fillin()
    closes #787
    
    Change-Id: I26ec7ebef74dad03192019b788e7c7ae17715783

---
+++ lib/dtmf_rx_fillin-02.h
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <spandsp/telephony.h>
+#include <spandsp/super_tone_rx.h>
+#include <spandsp/logging.h>
+#include <spandsp/dtmf.h>
+#include "compat.h"
+
+INLINE void dtmf_rx_fillin(dtmf_rx_state_t *dsp, int n) {
+	// stub
+}
