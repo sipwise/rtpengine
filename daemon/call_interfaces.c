9efcfe75cf365af937221641ba3ba53803c5c75c
Richard Fuchs <rfuchs@sipwise.com>
2019-08-06 13:57:00 -0400

    TT#64259 WIP
    Change-Id: I9fc8e1beb22690245a598fb3c082b23d0ce8c734

--- daemon/call_interfaces.c
+++ daemon/call_interfaces.c
@@ -28,6 +28,7 @@
 #include "main.h"
 #include "load.h"
 #include "media_player.h"
+#include "dtmf.h"
 
 
 static pcre *info_re;
@@ -694,6 +695,9 @@ static void call_ng_flags_flags(struct sdp_ng_flags *out, str *s, void *dummy) {
 			break;
 		case CSH_LOOKUP("asymmetric-codecs"):
 			out->asymmetric_codecs = 1;
+			break;
+		case CSH_LOOKUP("inject-dtmf"):
+			out->inject_dtmf = 1;
 			break;
 		case CSH_LOOKUP("pad-crypto"):
 			out->pad_crypto = 1;
@@ -1823,6 +1827,70 @@ const char *call_stop_media_ng(bencode_item_t *input, bencode_item_t *output) {
 		return "Not currently playing media";
 
 	media_player_stop(monologue->player);
+
+	err = NULL;
+
+out:
+	if (call) {
+		rwlock_unlock_w(&call->master_lock);
+		obj_put(call);
+	}
+	return err;
+#else
+	return "unsupported";
+#endif
+}
+
+
+const char *call_play_dtmf_ng(bencode_item_t *input, bencode_item_t *output) {
+#ifdef WITH_TRANSCODING
+	struct call *call;
+	struct call_monologue *monologue;
+	str str;
+	const char *err = NULL;
+
+	err = play_media_select_party(&call, &monologue, input);
+	if (err)
+		goto out;
+
+	long long duration = bencode_dictionary_get_int_str(input, "duration", 250);
+	if (duration < 100) {
+		duration = 100;
+		ilog(LOG_WARN, "Invalid duration (%lli ms) specified, using 100 ms instead", duration);
+	}
+	else if (duration > 5000) {
+		duration = 5000;
+		ilog(LOG_WARN, "Invalid duration (%lli ms) specified, using 5000 ms instead", duration);
+	}
+
+	long long code = bencode_dictionary_get_int_str(input, "code", -1);
+	err = "Out of range 'code' specified";
+	if (code == -1) {
+		// try a string code
+		err = "No valid 'code' specified";
+		if (bencode_dictionary_get_str(input, "code", &str))
+			goto out;
+		err = "Given 'code' is not a single digit";
+		if (str.len != 1)
+			goto out;
+		code = dtmf_code_from_char(str.s[0]);
+		err = "Invalid 'code' character";
+		if (code == -1)
+			goto out;
+	}
+	else if (code < 0)
+		goto out;
+	else if (code > 15)
+		goto out;
+
+	long long volume = bencode_dictionary_get_int_str(input, "volume", 8);
+	if (volume < 0)
+		volume *= -1;
+
+	ilog(LOG_DEBUG, "Injecting DTMF tone #%lli for %lli ms (vol %lli) towards '" STR_FORMAT "'",
+			code, duration, volume, STR_FMT(&monologue->tag));
+
+	// XXX
 
 	err = NULL;
 
