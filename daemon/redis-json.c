4cd817caa63e2931f302b1c59d77df13be67c787
Oded Arbel <oded@geek.co.il>
2020-01-20 21:51:43 +0200

    more error logs

--- daemon/redis-json.c
+++ daemon/redis-json.c
@@ -3,8 +3,6 @@
 #include "json-helpers.h"
 #include "log_funcs.h"
 
-#define rlog(l, x...) printf(x)
-
 #define JSON_UPDATE_NUM_FIELD_IF_SET(json, key, field) {\
 	long long llval = json_object_get_ll(json, key); \
 	if (llval >= 0) field = llval; \
@@ -235,18 +233,24 @@ static GQueue *redis_call_media_read_payloads(JsonArray* payload_types) {
 	str ptype;
 	unsigned idx, pt;
 
+	char *err = 0;
+
 	/* read payloads */
 	out = g_queue_new();
 	payload_count = json_array_get_length(payload_types);
 	for (idx = 0; idx < payload_count; idx++) {
 		payload_str = json_array_get_str(payload_types, idx);
-		if (str_token(&ptype, payload_str, '/'))
+		if (str_token(&ptype, payload_str, '/')) {
+			err = "No payload type in payload data";
 			goto fail;
+		}
 
 		pt = str_to_ui(&ptype, 0);
 		payload = redis_call_rtp_payload_type_create(pt, payload_str);
-		if (!payload)
+		if (!payload) {
+			err = "Failed to create payload";
 			goto fail;
+		}
 		g_queue_push_tail(out, payload);
 
 		free(payload_str);
@@ -259,6 +263,9 @@ fail:
 	if (out) {
 		g_queue_free_full(out, gdestroy_obj_put);
 		out = NULL;
+	}
+	if (err) {
+		ilog(LOG_WARNING, "Failed to read call data from Redis: %s", err);
 	}
 
 done:
@@ -275,6 +282,7 @@ static redis_call_media_t *redis_call_media_create(unsigned unique_id, JsonObjec
 	redis_call_media_tag_t *tagref = NULL;
 	redis_call_media_stream_t *streamref = NULL;
 
+	char *err = 0;
 	long long llval = 0;
 	unsigned idx;
 
@@ -282,8 +290,10 @@ static redis_call_media_t *redis_call_media_create(unsigned unique_id, JsonObjec
 	mediaref->unique_id = unique_id;
 	if ((llval = json_object_get_ll(json, "tag")) >= 0) {
 		tagref = g_queue_peek_nth(tags, llval);
-		if (!tagref)
+		if (!tagref) {
+			err = "Failed to find referenced tag when creating media";
 			goto fail;
+		}
 		mediaref->tag = obj_get(tagref);
 	}
 	JSON_UPDATE_NUM_FIELD_IF_SET(json, "index", mediaref->index);
@@ -303,10 +313,14 @@ static redis_call_media_t *redis_call_media_create(unsigned unique_id, JsonObjec
 			g_queue_push_tail(mediaref->streams, obj_get(streamref));
 	}
 
-	if (!(mediaref->codec_prefs_recv = redis_call_media_read_payloads(payload_types_recv_ar)))
+	if (!(mediaref->codec_prefs_recv = redis_call_media_read_payloads(payload_types_recv_ar))) {
+		err = "Failed to read recv payloads";
 		goto fail;
-	if (!(mediaref->codec_prefs_send = redis_call_media_read_payloads(payload_types_send_ar)))
+	}
+	if (!(mediaref->codec_prefs_send = redis_call_media_read_payloads(payload_types_send_ar))) {
+		err = "Failed to read send payloads";
 		goto fail;
+	}
 
 	goto done;
 
@@ -314,6 +328,9 @@ fail:
 	if (mediaref) {
 		obj_put(mediaref);
 		mediaref = NULL;
+	}
+	if (err) {
+		ilog(LOG_WARNING, "Failed to read call data from Redis: %s", err);
 	}
 
 done:
@@ -368,17 +385,25 @@ static int redis_call_match_tags(redis_call_media_tag_t *tag, GQueue *call_tags,
 	int status = 1;
 	unsigned num_others, other_idx, other_tagid;
 
+	char *err = 0;
+
 	num_others = json_array_get_length(json);
-	if (num_others < 0)
+	if (num_others < 0) {
+		err = "Negative number of other tags members?!?";
 		goto fail;
+	}
 
 	for (other_idx = 0; other_idx < num_others; other_idx++) {
 		other_tagid = json_array_get_ll(json, other_idx);
-		if (other_tagid < 0)
+		if (other_tagid < 0) {
+			err = "Failed to read other tag id from tag list";
 			goto fail;
+		}
 		other_tag = g_queue_peek_nth(call_tags, other_tagid);
-		if (!other_tag)
+		if (!other_tag) {
+			err = "Other tag id doesn't exist in tag list!";
 			goto fail;
+		}
 		tag->other_tag = obj_get(other_tag);
 		other_tag->other_tag = obj_get(tag);
 	}
@@ -387,6 +412,9 @@ static int redis_call_match_tags(redis_call_media_tag_t *tag, GQueue *call_tags,
 
 fail:
 	status = 0;
+	if (err) {
+		ilog(LOG_WARNING, "Failed to read call data from Redis: %s", err);
+	}
 
 done:
 	return status;
@@ -400,6 +428,8 @@ static GQueue *redis_call_read_tags(JsonObject *json) {
 	JsonObject *tag_object = NULL;
 	JsonArray *othertags_ar = NULL;
 
+	char *err = 0;
+
 	call_tags = g_queue_new();
 	for (tag_idx = 0; ; tag_idx++) {
 		tag_field = str_sprintf("tag-%u", tag_idx);
@@ -407,8 +437,10 @@ static GQueue *redis_call_read_tags(JsonObject *json) {
 		if (!tag_object)
 			break; /* no more tags */
 		tag = redis_call_media_tag_create(tag_idx, tag_object);
-		if (!tag)
+		if (!tag) {
+			err = "Failed to create media tag";
 			goto fail;
+		}
 		g_queue_push_tail(call_tags, tag);
 		free(tag_field);
 	}
@@ -419,10 +451,14 @@ static GQueue *redis_call_read_tags(JsonObject *json) {
 		othertags_ar = json_object_get_array_member(json, tag_field->s);
 		if (othertags_ar) {
 			tag = g_queue_peek_nth(call_tags, other_tags_idx);
-			if (!tag) /* shouldn't actually happen, but we're sanity-first! */
+			if (!tag) {/* shouldn't actually happen, but we're sanity-first! */
+				err = "The world is insane - no matching tag and other tag";
 				goto fail;
-			if (!redis_call_match_tags(tag, call_tags, othertags_ar))
+			}
+			if (!redis_call_match_tags(tag, call_tags, othertags_ar)) {
+				err = "Failed to match call tags and other tags";
 				goto fail;
+			}
 		} /* missing other_tags list is treated like an empty list */
 	}
 
@@ -432,6 +468,9 @@ fail:
 	if (call_tags) {
 		g_queue_free_full(call_tags, gdestroy_obj_put);
 		call_tags = NULL;
+	}
+	if (err) {
+		ilog(LOG_WARNING, "Failed to read call data from Redis: %s", err);
 	}
 
 done:
@@ -447,6 +486,8 @@ static GQueue *redis_call_read_stream_fds(JsonObject *json) {
 	JsonObject *sfd_object;
 	redis_call_media_stream_fd_t* sfd;
 
+	char *err = 0;
+
 	call_sfds = g_queue_new();
 	for (sfd_idx = 0; ; sfd_idx++) {
 		sfd_field = str_sprintf("sfd-%u", sfd_idx);
@@ -454,8 +495,10 @@ static GQueue *redis_call_read_stream_fds(JsonObject *json) {
 		if (!sfd_object)
 			goto done; /* no more sfds */
 		sfd = redis_call_media_stream_fd_create(sfd_idx, sfd_object);
-		if (!sfd)
+		if (!sfd) {
+			err = "Failed to create stream fd";
 			goto fail;
+		}
 		g_queue_push_tail(call_sfds, sfd);
 		free(sfd_field);
 	}
@@ -468,6 +511,9 @@ fail:
 	if (call_sfds) {
 		g_queue_free_full(call_sfds, gdestroy_obj_put);
 		call_sfds = NULL;
+	}
+	if (err) {
+		ilog(LOG_WARNING, "Failed to read call data from Redis: %s", err);
 	}
 
 done:
@@ -483,8 +529,12 @@ static GQueue *redis_call_read_streams(JsonObject *json) {
 	JsonObject *stream_object = NULL;
 	redis_call_media_stream_t *stream;
 
-	if (!(call_sfds = redis_call_read_stream_fds(json)))
+	char *err = 0;
+
+	if (!(call_sfds = redis_call_read_stream_fds(json))) {
+		err = "Failed to read stream fds";
 		goto fail;
+	}
 	call_streams = g_queue_new();
 	for (stream_idx = 0; ; stream_idx++) {
 		stream_field = str_sprintf("stream-%u", stream_idx);
@@ -492,8 +542,10 @@ static GQueue *redis_call_read_streams(JsonObject *json) {
 		if (!stream_object)
 			goto done; /* no more streams */
 		stream = redis_call_media_stream_create(stream_idx, stream_object, call_sfds);
-		if (!stream)
+		if (!stream) {
+			err = "Failed to create call media stream";
 			goto fail;
+		}
 		g_queue_push_tail(call_streams, stream);
 		free(stream_field);
 	}
@@ -506,6 +558,9 @@ fail:
 	if (call_streams) {
 		g_queue_free_full(call_streams, gdestroy_obj_put);
 		call_streams = NULL;
+	}
+	if (err) {
+		ilog(LOG_WARNING, "Failed to read call data from Redis: %s", err);
 	}
 
 done:
@@ -523,12 +578,17 @@ static GQueue *redis_call_read_media(JsonObject *json) {
 	JsonArray *stream_ids_ar = NULL, *endpoint_maps_ar = NULL, *payload_types_recv_ar = NULL, *payload_types_send_ar = NULL;
 	redis_call_media_t *media = NULL;
 
+	char *err = 0;
 	char fieldname[50];
 
-	if (!(call_tags = redis_call_read_tags(json)))
+	if (!(call_tags = redis_call_read_tags(json))) {
+		err = "Failed to read call tags";
 		goto fail;
-	if (!(call_streams = redis_call_read_streams(json)))
+	}
+	if (!(call_streams = redis_call_read_streams(json))) {
+		err = "Failed to read call streams";
 		goto fail;
+	}
 	call_media = g_queue_new();
 	for (media_idx = 0; ; media_idx++) {
 		snprintf(fieldname, sizeof(fieldname), "media-%u", media_idx);
@@ -543,12 +603,16 @@ static GQueue *redis_call_read_media(JsonObject *json) {
 		payload_types_recv_ar = json_object_get_array_member(json, fieldname);
 		snprintf(fieldname, sizeof(fieldname), "payload_types_send-%u", media_idx);
 		payload_types_send_ar = json_object_get_array_member(json, fieldname);
-		if (!stream_ids_ar || !endpoint_maps_ar || !payload_types_recv_ar || !payload_types_send_ar)
+		if (!stream_ids_ar || !endpoint_maps_ar || !payload_types_recv_ar || !payload_types_send_ar) {
+			err = "Missing streams, maps or payloads";
 			goto fail;
+		}
 		media = redis_call_media_create(media_idx, media_object, call_tags, call_streams, stream_ids_ar,
 						endpoint_maps_ar, payload_types_recv_ar, payload_types_send_ar);
-		if (!media)
+		if (!media) {
+			err = "Failed to create call media";
 			goto fail;
+		}
 		g_queue_push_tail(call_media, media);
 	}
 
@@ -558,6 +622,9 @@ static GQueue *redis_call_read_media(JsonObject *json) {
 fail:
 	if (call_media)
 		g_queue_free_full(call_media, gdestroy_obj_put);
+	if (err) {
+		ilog(LOG_WARNING, "Failed to read call data from Redis: %s", err);
+	}
 
 done:
 	if (call_tags)
@@ -572,7 +639,7 @@ redis_call_t* redis_call_create(const str* callid, JsonNode* json) {
 	JsonObject *root = NULL;
 	JsonObject *metadata = NULL;
 
-	const char *err = 0;
+	char *err = 0;
 
 	root = json_node_get_object(json);
 	metadata = json_object_get_object_member(root, "json");
@@ -598,9 +665,8 @@ fail:
 		callref = NULL;
 	}
 	if (err) {
-		rlog(LOG_WARNING, "Failed to read call data '" STR_FORMAT_M "' from Redis: %s",
-		     STR_FMT_M(callid),
-		     err);
+		ilog(LOG_WARNING, "Failed to read call data '" STR_FORMAT_M "' from Redis: %s",
+		     STR_FMT_M(callid), err);
 	}
 
 done:
