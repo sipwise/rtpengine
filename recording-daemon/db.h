5c7de88aaf3b9ecd07f2efb5e9e2807ccdc7578f
Richard Fuchs <rfuchs@sipwise.com>
2020-02-03 11:02:18 -0500

    TT#75351 delete DB streams that have no corresponding file
    Change-Id: I07bdfd6f4dfbcef82d0eb11461672f211badde78
    (cherry picked from commit 92adeb0dbb6df83f745f12e46ebe01a20fe047c9)

--- recording-daemon/db.h
+++ recording-daemon/db.h
@@ -8,6 +8,7 @@ void db_do_call(metafile_t *);
 void db_close_call(metafile_t *);
 void db_do_stream(metafile_t *mf, output_t *op, const char *type, stream_t *, unsigned long ssrc);
 void db_close_stream(output_t *op);
+void db_delete_stream(output_t *op);
 void db_config_stream(output_t *op);
 
 
