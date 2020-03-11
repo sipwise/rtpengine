e537109829ee4bf924f64dd21f6d65eb7eeb09d1
Richard Fuchs <rfuchs@sipwise.com>
2020-03-11 12:10:15 -0400

    TT#76711 generalise media_player mechanism
    Change-Id: I940bd8c156691418ee0fe7743e9fade59ba8635a

--- include/codec.h
+++ include/codec.h
@@ -52,8 +52,8 @@ struct codec_packet {
 
 struct codec_handler *codec_handler_get(struct call_media *, int payload_type);
 void codec_handlers_free(struct call_media *);
-struct codec_handler *codec_handler_make_playback(struct rtp_payload_type *src_pt,
-		struct rtp_payload_type *dst_pt, unsigned long ts);
+struct codec_handler *codec_handler_make_playback(const struct rtp_payload_type *src_pt,
+		const struct rtp_payload_type *dst_pt, unsigned long ts);
 void codec_handler_free(struct codec_handler *handler);
 void ensure_codec_def(struct rtp_payload_type *pt, struct call_media *media);
 
