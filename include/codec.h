dba59542cfbcc594268c352ff320fb086c4eb944
Richard Fuchs <rfuchs@sipwise.com>
2020-03-11 11:51:48 -0400

    TT#76711 T.38 <> audio gateway
    Change-Id: I4552e07eee632fa730745410b08c3bf70ab67ab2

--- include/codec.h
+++ include/codec.h
@@ -18,6 +18,7 @@ struct ssrc_hash;
 struct sdp_ng_flags;
 struct codec_ssrc_handler;
 struct rtp_header;
+struct stream_params;
 
 
 typedef int codec_handler_func(struct codec_handler *, struct media_packet *);
@@ -35,8 +36,10 @@ struct codec_handler {
 
 	struct ssrc_hash *ssrc_hash;
 	struct codec_handler *output_handler; // == self, or other PT handler
+#ifdef WITH_TRANSCODING
 	int (*packet_encoded)(encoder_t *enc, void *u1, void *u2);
 	int (*packet_decoded)(decoder_t *, AVFrame *, void *, void *);
+#endif
 
 	// for media playback
 	struct codec_ssrc_handler *ssrc_handler;
@@ -54,7 +57,6 @@ struct codec_handler *codec_handler_get(struct call_media *, int payload_type);
 void codec_handlers_free(struct call_media *);
 struct codec_handler *codec_handler_make_playback(const struct rtp_payload_type *src_pt,
 		const struct rtp_payload_type *dst_pt, unsigned long ts);
-void codec_handler_free(struct codec_handler **handler);
 void ensure_codec_def(struct rtp_payload_type *pt, struct call_media *media);
 
 void codec_add_raw_packet(struct media_packet *mp);
@@ -76,7 +78,9 @@ void __rtp_payload_type_add_send(struct call_media *other_media, struct rtp_payl
 
 #ifdef WITH_TRANSCODING
 
-void codec_handlers_update(struct call_media *receiver, struct call_media *sink, const struct sdp_ng_flags *);
+void codec_handler_free(struct codec_handler **handler);
+void codec_handlers_update(struct call_media *receiver, struct call_media *sink, const struct sdp_ng_flags *,
+		const struct stream_params *);
 void codec_add_dtmf_event(struct codec_ssrc_handler *ch, int code, int level, uint64_t ts);
 uint64_t codec_last_dtmf_event(struct codec_ssrc_handler *ch);
 uint64_t codec_encoder_pts(struct codec_ssrc_handler *ch);
@@ -86,7 +90,8 @@ uint64_t codec_decoder_unskip_pts(struct codec_ssrc_handler *ch);
 #else
 
 INLINE void codec_handlers_update(struct call_media *receiver, struct call_media *sink,
-		const struct sdp_ng_flags *flags) { }
+		const struct sdp_ng_flags *flags, const struct stream_params *sp) { }
+INLINE void codec_handler_free(struct codec_handler **handler) { }
 
 #endif
 
