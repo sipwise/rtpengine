0bc652665d2914bc3e1a3efc531b0f27f32c6383
Richard Fuchs <rfuchs@sipwise.com>
2020-03-11 11:51:48 -0400

    TT#76711 T.38 decoder - WIP
    Change-Id: I4552e07eee632fa730745410b08c3bf70ab67ab2

--- daemon/media_socket.c
+++ daemon/media_socket.c
@@ -1811,7 +1811,7 @@ static int stream_packet(struct packet_handler_ctx *phc) {
 	if (G_UNLIKELY(!phc->sink || !phc->sink->selected_sfd || !phc->out_srtp
 				|| !phc->out_srtp->selected_sfd || !phc->in_srtp->selected_sfd))
 	{
-		ilog(LOG_WARNING, "RTP packet from %s%s%s discarded", FMT_M(endpoint_print_buf(&phc->mp.fsin)));
+		ilog(LOG_WARNING, "Media packet from %s%s%s discarded", FMT_M(endpoint_print_buf(&phc->mp.fsin)));
 		atomic64_inc(&phc->mp.stream->stats.errors);
 		atomic64_inc(&rtpe_statsps.errors);
 		goto out;
