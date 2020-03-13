7c2260cd95589e3f158c43ed27a6548731313e15
Richard Fuchs <rfuchs@sipwise.com>
2020-03-13 08:07:21 -0400

    TT#76368 remove some graphite functions from global namespace
    Change-Id: Ic24778e9d04e015012e9d7cc873203d0dfbacaa6
    (cherry picked from commit 10ab85b3e59c6ed7d47e7c99f62491bc236cb03f)

--- include/graphite.h
+++ include/graphite.h
@@ -18,9 +18,6 @@ enum connection_state {
 
 extern struct timeval rtpe_latest_graphite_interval_start;
 
-int connect_to_graphite_server(const endpoint_t *ep);
-int send_graphite_data(struct totalstats *sent_data);
-void graphite_loop_run(endpoint_t *graphite_ep, int seconds);
 void set_prefix(char* prefix);
 void graphite_loop(void *d);
 void set_latest_graphite_interval_start(struct timeval *tv);
