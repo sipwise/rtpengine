cb9f6f9d777c9f73ee850747f6bd1063b886ee2e
Richard Fuchs <rfuchs@sipwise.com>
2020-07-23 08:45:51 -0400

    Recrypt SRTP for DTMF logging
    Change-Id: Id877b8464e99cfd5dca728a01fcdc3ffc654d698
    (cherry picked from commit e04fb03787c65615a56582232b2ea9b15d6acb63)

--- include/dtmf.h
+++ include/dtmf.h
@@ -23,6 +23,7 @@ int dtmf_event_payload(str *, uint64_t *, uint64_t, struct dtmf_event *, GQueue 
 void dtmf_event_free(void *);
 int dtmf_code_from_char(char);
 const char *dtmf_inject(struct call_media *media, int code, int volume, int duration, int pause);
+int dtmf_do_logging(void);
 
 
 #endif
