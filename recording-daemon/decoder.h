7815a4ba5cdf51dfc33a9540ccc2c83a6c160c1f
Richard Fuchs <rfuchs@sipwise.com>
2020-11-20 08:43:48 -0500

    TT#102450 fix AMR recording
    closes #1112
    
    Change-Id: If8378fa63ae1827aa3ab4c02d376ee75f34b8b9d
    (cherry picked from commit a6022c9ab9b9955774218e473033f098b6c6b59d)

--- recording-daemon/decoder.h
+++ recording-daemon/decoder.h
@@ -8,7 +8,7 @@
 extern int resample_audio;
 
 
-decode_t *decoder_new(const char *payload_str, int ptime, output_t *);
+decode_t *decoder_new(const char *payload_str, const char *format, int ptime, output_t *);
 int decoder_input(decode_t *, const str *, unsigned long ts, ssrc_t *);
 void decoder_free(decode_t *);
 
