1c94b74df2880263042aba804e3005dc01ff4217
Richard Fuchs <rfuchs@sipwise.com>
2021-04-27 11:14:12 -0400

    TT#14008 amend kernel test script
    Change-Id: If2031ad6e5c581775b96fa48d8b2d4a4c331ef19
    (cherry picked from commit a9fdc219f063055d68a5a69e3315b4371ed1f4a7)

--- t/test-kernel-module.c
+++ t/test-kernel-module.c
@@ -13,28 +13,40 @@ int main(void) {
 	reti = (struct rtpengine_target_info) {
 		.local = {
 			.family = AF_INET,
-			.u.ipv4 = 0x7f000001,
+			.u.ipv4 = htonl(0x7f000001),
 			.port = 4444,
 		},
 		.expected_src = {
 			.family = AF_INET,
-			.u.ipv4 = 0x7f000001,
+			.u.ipv4 = htonl(0x7f000001),
 			.port = 5555,
 		},
 		.src_mismatch = MSM_IGNORE,
 		.src_addr = {
 			.family = AF_INET,
-			.u.ipv4 = 0x7f000001,
+			.u.ipv4 = htonl(0x7f000001),
 			.port = 6666,
 		},
 		.dst_addr = {
 			.family = AF_INET,
-			.u.ipv4 = 0x7f000001,
+			.u.ipv4 = htonl(0x7f000001),
 			.port = 7777,
 		},
 		.decrypt = {
-			.cipher = REC_NULL,
+			.cipher = REC_AEAD_AES_GCM_256,
 			.hmac = REH_NULL,
+			.master_key_len = 32,
+			.master_salt_len = 12,
+			.session_key_len = 32,
+			.session_salt_len = 12,
+			.master_key = {
+				0xb8, 0x9a, 0x55, 0xee, 0xd9, 0xb6, 0x6f, 0xca, 0x76, 0xd3, 0x2a, 0x7e,
+				0x49, 0xfb, 0x8e, 0xa7, 0xff, 0xa7, 0xa9, 0xcd, 0x46, 0x42, 0x93, 0xf2,
+				0xd6, 0x23, 0x2c, 0x65, 0x77, 0x06, 0xdc, 0x71
+			},
+			.master_salt = {
+				0xc5, 0xd5, 0x69, 0x14, 0x90, 0x6a, 0x15, 0xfc, 0x52, 0x7b, 0x0a, 0xdb
+			},
 		},
 		.encrypt = {
 			.cipher = REC_AEAD_AES_GCM_256,
@@ -53,10 +65,34 @@ int main(void) {
 			},
 		},
 		.rtp = 1,
+		.rtcp_mux = 1,
+		.dtls = 1,
+		.stun = 1,
+		.ssrc = 0x12345678,
+		.ssrc_out = 0x54321,
+		.num_payload_types = 8,
+		.payload_types = { 97, 98, 120, 121, 124, 125, 126, 127, 0, },
+		.clock_rates = { 90000, 90000, 90000, 90000, 90000, 90000, 90000, 90000, 0, },
 	};
 
 	ret = kernel_add_stream(&reti);
 	assert(ret == 0);
 
+	reti.local.port = 4446;
+	ret = kernel_add_stream(&reti);
+	assert(ret == 0);
+
+	reti.local.port = 4448;
+	ret = kernel_add_stream(&reti);
+	assert(ret == 0);
+
+	reti.local.port = 4450;
+	ret = kernel_add_stream(&reti);
+	assert(ret == 0);
+
 	return 0;
+}
+
+int get_local_log_level(unsigned int u) {
+	return -1;
 }
