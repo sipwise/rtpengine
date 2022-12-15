c7a99099930df2b5f9656c1e6a22908911392e39
Denial <dhaq@evisi.co.id>
2022-12-15 16:58:47 +0700

    add mix_num_inputs variable to config file

--- recording-daemon/metafile.c
+++ recording-daemon/metafile.c
@@ -77,7 +77,6 @@ static void meta_destroy(metafile_t *mf) {
 
 // mf is locked
 static void meta_stream_interface(metafile_t *mf, unsigned long snum, char *content) {
-	dbg("Hello");
 	db_do_call(mf);
 	if (output_enabled && output_mixed && mf->recording_on) {
 		pthread_mutex_lock(&mf->mix_lock);
