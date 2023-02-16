5bd6ae218ebd72e96da6938173a8c0a600e6f3b8
Donat Zenichev <dzenichev@sipwise.com>
2023-02-16 19:49:02 +0100

    MT#56128 Temporarily remove the test #1142
    The test number 1142:
    "SDP attr manipulations - add a= line for media audio, two times"
    fails on the buster builds, because of the hash tables implementation
    used in the `command_values` of the `struct sdp_command`.
    
    It doesn't necessarily keep the same order, as was used to add values.
    
    Temporarily removing it, until the `struct sdp_command` is re-designed.
    
    Change-Id: I077771c00c5dc8907e42d7757540e3ffa2063af6

--- t/auto-daemon-tests.pl
+++ t/auto-daemon-tests.pl
@@ -15545,50 +15545,6 @@ SDP
 
 new_call;
 
-offer('SDP attr manipulations - add a= line for media audio, two times', { ICE => 'remove', DTLS => 'off', SDES => [ 'nonew' ], 'sdp-attr' => { audio => { add => ['test1', 'test2'] } }}, <<SDP);
-v=0
-o=- 1545997027 1 IN IP4 198.51.100.1
-s=tester
-t=0 0
-m=audio 2000 RTP/SAVP 0
-c=IN IP4 198.51.100.1
-a=sendrecv
-----------------------------------
-v=0
-o=- 1545997027 1 IN IP4 198.51.100.1
-s=tester
-t=0 0
-m=audio PORT RTP/SAVP 0
-c=IN IP4 203.0.113.1
-a=rtpmap:0 PCMU/8000
-a=sendrecv
-a=rtcp:PORT
-a=test1
-a=test2
-SDP
-
-answer('SDP attr manipulations - add a= line for media audio, two times', { ICE => 'remove' }, <<SDP);
-v=0
-o=- 1545997027 1 IN IP4 198.51.100.3
-s=tester
-t=0 0
-m=audio 2002 RTP/SAVP 0
-c=IN IP4 198.51.100.3
-a=sendrecv
---------------------------------------
-v=0
-o=- 1545997027 1 IN IP4 198.51.100.3
-s=tester
-t=0 0
-m=audio PORT RTP/SAVP 0
-c=IN IP4 203.0.113.1
-a=rtpmap:0 PCMU/8000
-a=sendrecv
-a=rtcp:PORT
-SDP
-
-new_call;
-
 offer('SDP attr manipulations - add a= line for media audio and remove one', { ICE => 'remove', DTLS => 'off', SDES => [ 'nonew' ], 'sdp-attr' => { audio => { add => ['test1'], remove => ['test2'] } }}, <<SDP);
 v=0
 o=- 1545997027 1 IN IP4 198.51.100.1
