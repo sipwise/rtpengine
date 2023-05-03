a6afc755ebe809f6cef44ed225586b82034466c6
Richard Fuchs <rfuchs@sipwise.com>
2023-05-03 10:18:24 -0400

    MT#57302 add `exclude-recording` flag
    Change-Id: I08cf87ce94720f3e01bfbe3260bdc76da8e1bb63
    (cherry picked from commit e02e03729c558e3b67d5c18b361e89013822b4e5)

--- include/call.h
+++ include/call.h
@@ -490,6 +490,7 @@ struct call_monologue {
 	unsigned int		rec_forwarding:1;
 	unsigned int		inject_dtmf:1;
 	unsigned int		detect_dtmf:1;
+	unsigned int		no_recording:1;
 };
 
 struct call_iterator_list {
