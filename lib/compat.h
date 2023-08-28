4b2dff15287951d17bf190cd9df9f041aa98e811
Richard Fuchs <rfuchs@sipwise.com>
2023-08-28 13:13:41 -0400

    MT#55283 fix warnings for older/non-gcc compilers
    ... which don't know about __attribute__((access))
    
    Change-Id: I30a31d03ab492e0e9cd2f7e59a292ad7ba9b184e

--- lib/compat.h
+++ lib/compat.h
@@ -7,6 +7,14 @@
 # define INLINE static inline __attribute__((always_inline))
 #endif
 
+#define ACCESS(...)
+#if defined __has_attribute
+# if __has_attribute(access)
+#  undef ACCESS
+#  define ACCESS(...) __attribute__((access(__VA_ARGS__)))
+# endif
+#endif
+
 
 #ifndef BENCODE_MALLOC
 #define BENCODE_MALLOC malloc
