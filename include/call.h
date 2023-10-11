88ac7e8168a19c7d9f63fef2832fcc5eb94f8173
Richard Fuchs <rfuchs@sipwise.com>
2023-10-11 10:06:38 -0400

    MT#55283 add desired_family to call_monologue
    This is required to look up the correct interface during Redis restore.
    
    Change-Id: I0ea67576deba95b4201c6384fdcf8051b84f879e
    (cherry picked from commit 531e720fd41427780621ccaf108e70a40cf2942b)

--- include/call.h
+++ include/call.h
@@ -451,6 +451,7 @@ struct call_monologue {
 	struct timeval		started; /* for CDR */
 	struct timeval		terminated; /* for CDR */
 	enum termination_reason	term_reason;
+	sockfamily_t		*desired_family;
 	const struct logical_intf *logical_intf;
 	GHashTable		*other_tags;
 	GHashTable		*branches;
