c96755de063adc3383870fb34139303b6b4389ed
Richard Fuchs <rfuchs@sipwise.com>
2024-02-08 12:32:26 -0500

    MT#55283 set up recording after offer/answer
    The monologue label (and possibly other) get set during the offer/answer
    routine. Do the call recording setup after this has been done.
    
    closes #1791
    
    Change-Id: Ie1b1a7d5a1df7058ba90c3dd5cbd79159f48683d

--- daemon/call_interfaces.c
+++ daemon/call_interfaces.c
@@ -2156,9 +2156,6 @@ static const char *call_offer_answer_ng(ng_buffer *ngbuf, bencode_item_t *input,
 	chopper = sdp_chopper_new(&sdp);
 	bencode_buffer_destroy_add(output->buffer, (free_func_t) sdp_chopper_destroy, chopper);
 
-	update_metadata_monologue(from_ml, &flags);
-	detect_setup_recording(call, &flags);
-
 	if (flags.drop_traffic_start) {
 		CALL_SET(call, DROP_TRAFFIC);
 	}
@@ -2174,6 +2171,9 @@ static const char *call_offer_answer_ng(ng_buffer *ngbuf, bencode_item_t *input,
 		ret = sdp_replace(chopper, &parsed, to_ml, &flags);
 	if (!ret)
 		save_last_sdp(from_ml, &sdp, &parsed, &streams);
+
+	update_metadata_monologue(from_ml, &flags);
+	detect_setup_recording(call, &flags);
 
 	struct recording *recording = call->recording;
 	if (recording != NULL) {
