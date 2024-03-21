sipwise/rtpengine

commit d7b8ebbc687ea840913ecb6fa6f020df901e8fb9

MT#58535 Add an empty statement after `rtpp-flags` parsing
A fix for bullseye/buster and focal:

call_interfaces.c: In function 'call_ng_main_flags':
call_interfaces.c:1828:4: error: a label can only be part of a statement and a declaration is not a statement
 1828 |    bencode_item_t * dict;
      |    ^~~~~~~~~~~~~~
make[3]: *** [<builtin>: call_interfaces.strhash.o] Error 1

Switch case has to be followed by a statement, and the declaration
is not, so just follow it by the empty statement.

Change-Id: I7c5ef62dec9ceb404736ec3c4bed84f04b892d24

daemon/call_interfaces.c
@@ -1822,8 +1822,8 @@ static void call_ng_main_flags(sdp_ng_flags *out, str *key, bencode_item_t *valu
 		case CSH_LOOKUP("RTCP-mux"):
 			call_ng_flags_str_list(out, value, call_ng_flags_rtcp_mux, NULL);
 			break;
-		case CSH_LOOKUP("rtpp-flags"):
-		case CSH_LOOKUP("rtpp_flags"):
+		case CSH_LOOKUP("rtpp-flags"):;
+		case CSH_LOOKUP("rtpp_flags"):;
 			/* new dictionary to store rtpp_flags */
 			bencode_item_t * dict;
 			dict = bencode_dictionary(value->buffer);

commit d1c4b44f790c826e1bd96a1fd06fed9c8b014387

MT#58535 Fix a defect detected by Coverity Scan
Introduced by d7b8ebbc687ea840913ecb6fa6f020df901e8fb9

*** CID 1584501:  Control flow issues  (MISSING_BREAK)
/daemon/call_interfaces.c: 1825 in call_ng_main_flags()
1819                            call_ng_flags_str_list(out, value, call_ng_flags_replace, NULL);
1820                            break;
1821                    case CSH_LOOKUP("rtcp-mux"):
1822                    case CSH_LOOKUP("RTCP-mux"):
1823                            call_ng_flags_str_list(out, value, call_ng_flags_rtcp_mux, NULL);
1824                            break;
>>>     CID 1584501:  Control flow issues  (MISSING_BREAK)
>>>     The case for value "324" is not terminated by a "break" statement.
1825                    case CSH_LOOKUP("rtpp-flags"):;
1826                    case CSH_LOOKUP("rtpp_flags"):;
1827                            /* new dictionary to store rtpp_flags */
1828                            bencode_item_t * dict;
1829                            dict = bencode_dictionary(value->buffer);
1830                            assert(dict != NULL);

Change-Id: Iaa1d9b3bd31d32e48451c3a47f8e99fa3c755e63

daemon/call_interfaces.c
@@ -1822,7 +1822,7 @@ static void call_ng_main_flags(sdp_ng_flags *out, str *key, bencode_item_t *valu
 		case CSH_LOOKUP("RTCP-mux"):
 			call_ng_flags_str_list(out, value, call_ng_flags_rtcp_mux, NULL);
 			break;
-		case CSH_LOOKUP("rtpp-flags"):;
+		case CSH_LOOKUP("rtpp-flags"):
 		case CSH_LOOKUP("rtpp_flags"):;
 			/* new dictionary to store rtpp_flags */
 			bencode_item_t * dict;