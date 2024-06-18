236ca51ac8f11e7e10e031c3723ac79ece739b7a
Richard Fuchs <rfuchs@sipwise.com>
2024-06-18 13:05:38 -0400

    MT#55283 extend fake string hashing for IDE
    Change-Id: Ifbd4b615ca98a0b74e8d9e0732d2c5c208ecf819

--- daemon/.ycm_extra_conf.py
+++ daemon/.ycm_extra_conf.py
@@ -12,7 +12,7 @@ compilation_database_folder = ''
 repl_tmpl = '(int)((x)[a]?(((long)((x)[a])<<a)^!):0)'
 csh_lookup_str = '!'
 
-for num in range(30):
+for num in range(31):
     t_str = repl_tmpl.replace('a', str(num))
     csh_lookup_str = csh_lookup_str.replace('!', t_str)
 
