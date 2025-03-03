f413ab39df347929218b469f1f30a3e083096112
Richard Fuchs <rfuchs@sipwise.com>
2025-03-03 10:37:50 -0400

    MT#55283 remove NULL tests
    Empty subscriptions are not supported and would be an internal bug.
    Remove expectations that the contents can be NULL.
    
    Change-Id: Ibd7b0d86e7fb147135a93316575954aca546eac2

--- daemon/call.c
+++ daemon/call.c
@@ -2640,8 +2640,6 @@ static void __update_init_subscribers(struct call_media *media, struct stream_pa
 	{
 		struct media_subscription * ms = l->data;
 		struct call_media * sub_media = ms->media;
-		if (!sub_media)
-			continue;
 		if (!__streams_set_sinks(media, sub_media, flags, &ms->attrs))
 			ilog(LOG_WARN, "Error initialising streams");
 	}
@@ -3106,8 +3104,6 @@ int monologue_offer_answer(struct call_monologue *monologues[2], sdp_streams_q *
 		for (auto_iter(l, sender_media->media_subscriptions.head); l && !receiver_media; l = l->next) {
 			__auto_type ms = l->data;
 			__auto_type r_media = ms->media;
-			if (!r_media)
-				continue;
 			if (r_media->monologue != receiver_ml)
 				continue;
 			/* check type, it must match */
@@ -3708,9 +3704,6 @@ int monologue_subscribe_answer(struct call_monologue *dst_ml, sdp_ng_flags *flag
 		struct media_subscription * ms = src_ml_media_it->data;
 		struct call_media * src_media = ms->media;
 
-		if (!src_media)
-			continue;
-
 		rev_ms = call_get_media_subscription(src_media->media_subscribers_ht, dst_media);
 		if (rev_ms)
 			rev_ms->attrs.transcoding = 0;
@@ -3793,9 +3786,6 @@ int monologue_unsubscribe(struct call_monologue *dst_ml, sdp_ng_flags *flags) {
 			__auto_type next = l->next;
 			struct media_subscription * ms = l->data;
 			struct call_media * src_media = ms->media;
-
-			if (!src_media)
-				continue;
 
 			__media_unconfirm(src_media, "media unsubscribe");
 			__unsubscribe_media_link(media, l);
@@ -4767,15 +4757,11 @@ void dialogue_unconfirm(struct call_monologue *ml, const char *reason) {
 		for (__auto_type l = media->media_subscriptions.head; l; l = l->next)
 		{
 			struct media_subscription * ms = l->data;
-			if (!ms->media)
-				continue;
 			__media_unconfirm(ms->media, reason);
 		}
 		for (__auto_type l = media->media_subscribers.head; l; l = l->next)
 		{
 			struct media_subscription * ms = l->data;
-			if (!ms->media)
-				continue;
 			__media_unconfirm(ms->media, reason);
 		}
 	}
