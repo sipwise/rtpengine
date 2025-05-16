1937fbfd79010165d5a580a85af9c84f12515a36
Tom Briden <tom.briden@eckoh.com>
2025-05-16 08:23:58 +0100

    add flag to discard delay buffer packets in codec_handlers_stop before media playback
    if a delay buffer is enabled, the packets get scheduled in the future,
    but media playback packets are scheduled immediately. this results
    in choppy playback due to the packets coming in at the same time.
    This change adds a new bool to sdp_ng_flags that gets set when
    play-media is called and passed in to codec_handlers_stop so the delay
    buffer queue is cleared instead of flushed

--- daemon/codec.c
+++ daemon/codec.c
@@ -1466,7 +1466,7 @@ void __codec_handlers_update(struct call_media *receiver, struct call_media *sin
 	if (proto_is_not_rtp(receiver->protocol)) {
 		__generator_stop_all(receiver);
 		__generator_stop_all(sink);
-		codec_handlers_stop(&receiver->codec_handlers_store, sink);
+		codec_handlers_stop(&receiver->codec_handlers_store, sink, true);
 		return;
 	}
 
@@ -1482,7 +1482,10 @@ void __codec_handlers_update(struct call_media *receiver, struct call_media *sin
 	// we're doing some kind of media passthrough - shut down local generators
 	__generator_stop(receiver);
 	__generator_stop(sink);
-	codec_handlers_stop(&receiver->codec_handlers_store, sink);
+
+	// flush_delay_buffer is disabled by play-media to ensure media packets don't get scheduled
+	// alongside the delay buffer packets resulting in choppy audio
+	codec_handlers_stop(&receiver->codec_handlers_store, sink, a.flags ? a.flags->flush_delay_buffer : true);
 	// XXX this can cause unnecessary shutdown/resets of codec handlers
 
 	bool is_transcoding = false;
@@ -4158,7 +4161,7 @@ static void __ssrc_handler_stop(void *p, void *arg) {
 	}
 	codec_cc_stop(ch->chain);
 }
-void codec_handlers_stop(codec_handlers_q *q, struct call_media *sink) {
+void codec_handlers_stop(codec_handlers_q *q, struct call_media *sink, bool flush_delay_buffer) {
 	for (__auto_type l = q->head; l; l = l->next) {
 		struct codec_handler *h = l->data;
 
@@ -4167,7 +4170,7 @@ void codec_handlers_stop(codec_handlers_q *q, struct call_media *sink) {
 
 		if (h->delay_buffer) {
 			mutex_lock(&h->delay_buffer->lock);
-			__delay_buffer_shutdown(h->delay_buffer, true);
+			__delay_buffer_shutdown(h->delay_buffer, flush_delay_buffer);
 			mutex_unlock(&h->delay_buffer->lock);
 
 			delay_buffer_stop(&h->delay_buffer);
