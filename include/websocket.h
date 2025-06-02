bc94e052199f2ffa6172abb09b40ff07d4ad0181
Richard Fuchs <rfuchs@sipwise.com>
2025-06-02 15:56:36 -0400

    MT#55283 overhaul WS locking logic
    Multiple writers may operate on a single WS connection simultaneously.
    Make sure they don't get in each other's way while constructing their
    messages/responses by holding the lock from the beginning of the
    response until the point when it's fully ready. This fixes a problem of
    parts of multiple messages getting mixed up with each other.
    
    Change-Id: If84224fc06b423cd65c12981a5b09ee99b121df2
    (cherry picked from commit 148057400121a19a9891f22c4e3881f6736adbd4)
    (cherry picked from commit a40dea5007894c5bda0a57b20807da22e5b1800c)

--- include/websocket.h
+++ include/websocket.h
@@ -38,18 +38,15 @@ int websocket_init(void);
 void websocket_start(void);
 void websocket_stop(void);
 
-// appends to output buffer without triggering a response
-void websocket_queue_raw(struct websocket_conn *wc, const char *msg, size_t len);
-// adds data to output buffer (can be null) and optionally triggers specified response
-void websocket_write_raw(struct websocket_conn *wc, const char *msg, size_t len,
-		enum lws_write_protocol protocol, bool done);
 // adds data to output buffer (can be null) and triggers specified response: http or binary websocket
-void websocket_write_http_len(struct websocket_conn *wc, const char *msg, size_t len, bool done);
-void websocket_write_http(struct websocket_conn *wc, const char *msg, bool done);
-void websocket_write_text(struct websocket_conn *wc, const char *msg, bool done);
-void websocket_write_binary(struct websocket_conn *wc, const char *msg, size_t len, bool done);
-// num bytes in output buffer
-size_t websocket_queue_len(struct websocket_conn *wc);
+//void websocket_write_http_len(struct websocket_conn *wc, const char *msg, size_t len);
+//void websocket_write_http(struct websocket_conn *wc, const char *msg);
+void websocket_write_text(struct websocket_conn *wc, const char *msg);
+//void websocket_write_binary(struct websocket_conn *wc, const char *msg, size_t len);
+
+// single shot HTTP response
+void websocket_http_complete(struct websocket_conn *wc, int status, const char *content_type,
+		ssize_t content_length, const char *content);
 
 // write HTTP response headers
 void websocket_http_response(struct websocket_conn *wc, int status, const char *content_type,
