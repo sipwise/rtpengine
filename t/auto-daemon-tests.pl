ab61473add33cf2e8ed619283cc55b1c4a717d19
Richard Fuchs <rfuchs@sipwise.com>
2025-06-20 10:06:22 -0400

    MT#55283 add test for Ie233a4a45b
    Change-Id: If1aaa81931b976a3039b70c531a792fc2aed0e97
    (cherry picked from commit 4f664646b006642e8db4f3a56de9db8b9c87027c)

--- t/auto-daemon-tests.pl
+++ t/auto-daemon-tests.pl
@@ -84,6 +84,51 @@ sub stun_succ {
 
 
 
+new_call;
+
+offer('asymmetric codecs w transcoding', { codec => { transcode => ['G722'] } }, <<SDP);
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio 2000 RTP/AVP 0
+c=IN IP4 198.51.100.14
+a=sendrecv
+----------------------------------
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio PORT RTP/AVP 0 9
+c=IN IP4 203.0.113.1
+a=rtpmap:0 PCMU/8000
+a=rtpmap:9 G722/8000
+a=sendrecv
+a=rtcp:PORT
+SDP
+
+answer('asymmetric codecs w transcoding', { flags => ['allow asymmetric codecs'] }, <<SDP);
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio 2000 RTP/AVP 9
+c=IN IP4 198.51.100.14
+a=sendrecv
+----------------------------------
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio PORT RTP/AVP 0
+c=IN IP4 203.0.113.1
+a=rtpmap:0 PCMU/8000
+a=sendrecv
+a=rtcp:PORT
+SDP
+
+
+
 ($sock_a, $sock_ax, $sock_b, $sock_bx,
 $sock_c, $sock_cx, $sock_d, $sock_dx) = new_call([qw(198.51.100.35 3090)], [qw(198.51.100.35 3091)],
 							[qw(198.51.100.35 3092)], [qw(198.51.100.35 3093)],
