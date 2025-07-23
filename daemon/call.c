ebf6283f29261cbce42ab221a415985d5efa470a
Richard Fuchs <rfuchs@sipwise.com>
2025-07-23 08:17:07 -0400

    MT#55283 fix num_ports not being set
    Change-Id: I98b11a011063ae334118d977adc7b06fb9bff9c3
    (cherry picked from commit 8f13d83d2b74d9cd9116cc24d0d0339477755077)
    (cherry picked from commit 20a29777f4e9898af171f114df9005bfd8441960)

--- daemon/call.c
+++ daemon/call.c
@@ -782,10 +782,11 @@ static struct endpoint_map *__get_endpoint_map(struct call_media *media, unsigne
 		else
 			em->wildcard = 1;
 		em->logical_intf = media->logical_intf;
-		em->num_ports = num_ports;
 		g_queue_init(&em->intf_sfds);
 		g_queue_push_tail(&media->endpoint_maps, em);
 	}
+
+	em->num_ports = num_ports;
 
 	if (num_ports > 16)
 		return NULL;
