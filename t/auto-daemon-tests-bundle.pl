dbd09db24ec202db8a72bfd7298673f94b20147c
Richard Fuchs <rfuchs@sipwise.com>
2025-08-05 12:00:26 -0400

    MT#63317 add bundle tests
    Change-Id: I94641d57219d2b2425a17da4b5e4c01fa2f71512

---
+++ t/auto-daemon-tests-bundle.pl
@@ -0,0 +1,334 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use NGCP::Rtpengine::Test;
+use NGCP::Rtpclient::SRTP;
+use NGCP::Rtpengine::AutoTest;
+use Test::More;
+use POSIX;
+
+
+autotest_start(qw(--config-file=none -t -1 -i 203.0.113.1 -i 2001:db8:4321::1
+			-n 2223 -c 12345 -f -L 7 -E -u 2222 --log-level-internals=7))
+		or die;
+
+
+
+my ($sock_a, $sock_b, $sock_c, $sock_d, $port_a, $port_b, $port_c, $ssrc_a, $ssrc_b, $resp,
+	$sock_ax, $sock_bx, $port_ax, $port_bx, $port_d, $sock_e, $port_e, $sock_cx, $port_cx,
+	$srtp_ctx_a, $srtp_ctx_b, $srtp_ctx_a_rev, $srtp_ctx_b_rev, $ufrag_a, $ufrag_b,
+	@ret1, @ret2, @ret3, @ret4, $srtp_key_a, $srtp_key_b, $ts, $seq, $tag_medias, $media_labels,
+	$ftr, $ttr, $fts, $ttr2);
+
+
+
+($sock_a, $sock_b, $sock_c, $sock_d) =
+	new_call([qw(198.51.100.14 6150)],
+		[qw(198.51.100.14 6152)],
+		[qw(198.51.100.14 6154)],
+		[qw(198.51.100.14 6156)]);
+
+($port_a, undef, $port_b) = offer('control',
+	{ }, <<SDP);
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio 6150 RTP/AVP 0
+c=IN IP4 198.51.100.14
+a=sendrecv
+a=mid:a
+m=video 6152 RTP/AVP 105
+c=IN IP4 198.51.100.14
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=mid:v
+----------------------------------
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio PORT RTP/AVP 0
+c=IN IP4 203.0.113.1
+a=mid:a
+a=rtpmap:0 PCMU/8000
+a=sendrecv
+a=rtcp:PORT
+m=video PORT RTP/AVP 105
+c=IN IP4 203.0.113.1
+a=mid:v
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=rtcp:PORT
+SDP
+
+($port_c, undef, $port_d) = answer('control',
+	{ }, <<SDP);
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio 6154 RTP/AVP 0
+c=IN IP4 198.51.100.14
+a=sendrecv
+a=mid:a
+m=video 6156 RTP/AVP 105
+c=IN IP4 198.51.100.14
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=mid:v
+----------------------------------
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio PORT RTP/AVP 0
+c=IN IP4 203.0.113.1
+a=mid:a
+a=rtpmap:0 PCMU/8000
+a=sendrecv
+a=rtcp:PORT
+m=video PORT RTP/AVP 105
+c=IN IP4 203.0.113.1
+a=mid:v
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=rtcp:PORT
+SDP
+
+isnt($port_a, $port_b, 'different ports');
+isnt($port_a, $port_c, 'different ports');
+isnt($port_a, $port_d, 'different ports');
+isnt($port_b, $port_c, 'different ports');
+isnt($port_b, $port_d, 'different ports');
+isnt($port_c, $port_d, 'different ports');
+
+snd($sock_a, $port_c,  rtp(0, 2000, 6000, 0x1234, "\x44" x 160));
+rcv($sock_c, $port_a, rtpm(0, 2000, 6000, 0x1234, "\x44" x 160));
+
+snd($sock_c, $port_a,  rtp(0, 6000, 8000, 0x5678, "\x77" x 160));
+rcv($sock_a, $port_c, rtpm(0, 6000, 8000, 0x5678, "\x77" x 160));
+
+snd($sock_b, $port_d,  rtp(105, 3000, 4000, 0x6321, "\x33" x 800));
+rcv($sock_d, $port_b, rtpm(105, 3000, 4000, 0x6321, "\x33" x 800));
+
+snd($sock_d, $port_b,  rtp(105, 7000, 9000, 0x8741, "\x22" x 800));
+rcv($sock_b, $port_d, rtpm(105, 7000, 9000, 0x8741, "\x22" x 800));
+
+
+
+($sock_a, $sock_b, $sock_c, $sock_d) =
+	new_call([qw(198.51.100.14 6158)],
+		[qw(198.51.100.14 6160)],
+		[qw(198.51.100.14 6162)],
+		[qw(198.51.100.14 6164)]);
+
+($port_a, undef, $port_b) = offer('optional bundle offer',
+	{ }, <<SDP);
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+a=group:BUNDLE a v
+m=audio 6158 RTP/AVP 0
+c=IN IP4 198.51.100.14
+a=sendrecv
+a=mid:a
+m=video 6160 RTP/AVP 105
+c=IN IP4 198.51.100.14
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=mid:v
+----------------------------------
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio PORT RTP/AVP 0
+c=IN IP4 203.0.113.1
+a=mid:a
+a=rtpmap:0 PCMU/8000
+a=sendrecv
+a=rtcp:PORT
+m=video PORT RTP/AVP 105
+c=IN IP4 203.0.113.1
+a=mid:v
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=rtcp:PORT
+SDP
+
+($port_c, undef, $port_d) = answer('optional bundle offer',
+	{ }, <<SDP);
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio 6162 RTP/AVP 0
+c=IN IP4 198.51.100.14
+a=sendrecv
+a=mid:a
+m=video 6164 RTP/AVP 105
+c=IN IP4 198.51.100.14
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=mid:v
+----------------------------------
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio PORT RTP/AVP 0
+c=IN IP4 203.0.113.1
+a=mid:a
+a=rtpmap:0 PCMU/8000
+a=sendrecv
+a=rtcp:PORT
+m=video PORT RTP/AVP 105
+c=IN IP4 203.0.113.1
+a=mid:v
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=rtcp:PORT
+SDP
+
+isnt($port_a, $port_b, 'different ports');
+isnt($port_a, $port_c, 'different ports');
+isnt($port_a, $port_d, 'different ports');
+isnt($port_b, $port_c, 'different ports');
+isnt($port_b, $port_d, 'different ports');
+isnt($port_c, $port_d, 'different ports');
+
+snd($sock_a, $port_c,  rtp(0, 2000, 6000, 0x1234, "\x44" x 160));
+rcv($sock_c, $port_a, rtpm(0, 2000, 6000, 0x1234, "\x44" x 160));
+
+snd($sock_c, $port_a,  rtp(0, 6000, 8000, 0x5678, "\x77" x 160));
+rcv($sock_a, $port_c, rtpm(0, 6000, 8000, 0x5678, "\x77" x 160));
+
+snd($sock_b, $port_d,  rtp(105, 3000, 4000, 0x6321, "\x33" x 800));
+rcv($sock_d, $port_b, rtpm(105, 3000, 4000, 0x6321, "\x33" x 800));
+
+snd($sock_d, $port_b,  rtp(105, 7000, 9000, 0x8741, "\x22" x 800));
+rcv($sock_b, $port_d, rtpm(105, 7000, 9000, 0x8741, "\x22" x 800));
+
+
+# mix up bundle ports
+
+snd($sock_a, $port_d,  rtp(0, 2000, 6000, 0x1234, "\x44" x 160));
+rcv($sock_c, $port_a, rtpm(0, 2000, 6000, 0x1234, "\x44" x 160));
+
+snd($sock_b, $port_c,  rtp(105, 3000, 4000, 0x6321, "\x33" x 800));
+rcv($sock_d, $port_b, rtpm(105, 3000, 4000, 0x6321, "\x33" x 800));
+
+
+
+
+undef($sock_b);
+
+
+($sock_a, $sock_c, $sock_d) =
+	new_call([qw(198.51.100.14 6166)],
+		[qw(198.51.100.14 6168)],
+		[qw(198.51.100.14 6170)]);
+
+($port_a, undef, $port_b) = offer('same-port bundle offer',
+	{ }, <<SDP);
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+a=group:BUNDLE a v
+m=audio 6166 RTP/AVP 0
+c=IN IP4 198.51.100.14
+a=sendrecv
+a=mid:a
+m=video 6166 RTP/AVP 105
+c=IN IP4 198.51.100.14
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=mid:v
+----------------------------------
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio PORT RTP/AVP 0
+c=IN IP4 203.0.113.1
+a=mid:a
+a=rtpmap:0 PCMU/8000
+a=sendrecv
+a=rtcp:PORT
+m=video PORT RTP/AVP 105
+c=IN IP4 203.0.113.1
+a=mid:v
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=rtcp:PORT
+SDP
+
+($port_c, undef, $port_d) = answer('same-port bundle offer',
+	{ }, <<SDP);
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio 6168 RTP/AVP 0
+c=IN IP4 198.51.100.14
+a=sendrecv
+a=mid:a
+m=video 6170 RTP/AVP 105
+c=IN IP4 198.51.100.14
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=mid:v
+----------------------------------
+v=0
+o=- 1545997027 1 IN IP4 198.51.100.1
+s=tester
+t=0 0
+m=audio PORT RTP/AVP 0
+c=IN IP4 203.0.113.1
+a=mid:a
+a=rtpmap:0 PCMU/8000
+a=sendrecv
+a=rtcp:PORT
+m=video PORT RTP/AVP 105
+c=IN IP4 203.0.113.1
+a=mid:v
+a=rtpmap:105 H264/90000
+a=sendrecv
+a=rtcp:PORT
+SDP
+
+isnt($port_a, $port_b, 'different ports');
+isnt($port_a, $port_c, 'different ports');
+isnt($port_a, $port_d, 'different ports');
+isnt($port_b, $port_c, 'different ports');
+isnt($port_b, $port_d, 'different ports');
+isnt($port_c, $port_d, 'different ports');
+
+snd($sock_a, $port_c,  rtp(0, 2000, 6000, 0x1234, "\x44" x 160));
+rcv($sock_c, $port_a, rtpm(0, 2000, 6000, 0x1234, "\x44" x 160));
+
+snd($sock_c, $port_a,  rtp(0, 6000, 8000, 0x5678, "\x77" x 160));
+rcv($sock_a, $port_c, rtpm(0, 6000, 8000, 0x5678, "\x77" x 160));
+
+snd($sock_a, $port_d,  rtp(105, 3000, 4000, 0x6321, "\x33" x 800));
+rcv($sock_d, $port_b, rtpm(105, 3000, 4000, 0x6321, "\x33" x 800));
+
+snd($sock_d, $port_b,  rtp(105, 7000, 9000, 0x8741, "\x22" x 800));
+rcv($sock_a, $port_d, rtpm(105, 7000, 9000, 0x8741, "\x22" x 800));
+
+# mix up bundle ports
+
+snd($sock_a, $port_d,  rtp(0, 2000, 6000, 0x1234, "\x44" x 160));
+rcv($sock_c, $port_a, rtpm(0, 2000, 6000, 0x1234, "\x44" x 160));
+
+snd($sock_a, $port_c,  rtp(105, 3000, 4000, 0x6321, "\x33" x 800));
+rcv($sock_d, $port_b, rtpm(105, 3000, 4000, 0x6321, "\x33" x 800));
+
+
+
+#done_testing;NGCP::Rtpengine::AutoTest::terminate('f00');exit;
+done_testing();
