2639d9c0d1cebe27ea555a1bc3d40b16169a4a6c
Alex Brown <alex.brown@myphones.com>
2025-11-14 09:54:23 +0000

    MT#55283 Fix Issue with pcap based recording getting overwritten
    Closes #2025
    Closes #2026
    
    Change-Id: I9f49127725b867b5fc9e9a852e6688e2c1d10b49

--- daemon/recording.c
+++ daemon/recording.c
@@ -819,6 +819,10 @@ void recording_finish(call_t *call, bool discard) {
 
 	g_free(recording);
 	call->recording = NULL;
+
+	// clear the meta prefix to esure that pcaps for subsequent
+	// start recordings dont overwrite previous ones
+	call->recording_meta_prefix = STR_NULL;
 }
 
 
