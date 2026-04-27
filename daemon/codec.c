38d0f1504a2ba4cab723205894d199875362bc67
Richard Fuchs <rfuchs@sipwise.com>
2026-04-27 10:05:03 -0400

    MT#55283 extend search for compatible codec
    Change-Id: Ib1b9e08e65ff26c544d7deff99ab3c7d57cb4673

--- daemon/codec.c
+++ daemon/codec.c
@@ -6135,6 +6135,17 @@ static void codec_store_add_q(struct codec_store *cs, const str_q *offer, struct
 		}
 		GQueue *orig_list = orig ? t_hash_table_lookup(orig->codec_names, codec) : NULL;
 		if (!orig_list || !orig_list->length || cs->strip_full) {
+			// check for a compatible entry on opposite side
+			if (orig) {
+				codec_store_find_matching_codecs(NULL, &pt_match, orig, codec, pt);
+				if (pt_match) {
+					ilogs(codec, LOG_DEBUG, "Adding previously existing codec " STR_FORMAT,
+							STR_FMT(&pt_match->encoding_with_full_params));
+					codec_touched(cs, pt_match);
+					codec_store_add_order(cs, pt_match);
+					continue;
+				}
+			}
 			ilogs(codec, LOG_DEBUG, "Adding codec " STR_FORMAT
 					" for transcoding",
 					STR_FMT(codec));
