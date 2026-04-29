e2f0dd33dd34a8890e71b17fb67bfacfc6906a8c
Richard Fuchs <rfuchs@sipwise.com>
2026-04-29 12:00:55 -0400

    MT#55283 remove stray log message
    Change-Id: Iaf67d350b44bf48de096129e797c8d07b027b4a5
    (cherry picked from commit 175f1dfe34ad7aa9772cd527546f3d0aa0d73257)
    (cherry picked from commit 5f2acbb093d0deb683fc8d6192ec4b83d18ad41d)

--- kernel-module/nft_rtpengine.c
+++ kernel-module/nft_rtpengine.c
@@ -2487,7 +2487,6 @@ static int table_new_target(struct rtpengine_table *t, struct rtpengine_target_i
 		if (!i->num_destinations)
 			return -EINVAL;
 		for (u = 0; u < RTPE_NUM_OUTPUT_MEDIA; u++) {
-			printk(KERN_ERR "XXX %d %d\n", __LINE__, i->media_output_idxs[u].rtp_start_idx);
 			if (i->media_output_idxs[u].rtp_start_idx >= i->num_destinations)
 				return -EINVAL;
 			if (i->media_output_idxs[u].rtp_end_idx > i->num_destinations)
