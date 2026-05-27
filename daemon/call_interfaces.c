e438feacbf18290a8846d1a64d0cf77b9a29354b
Donat Zenichev <dzenichev@sipwise.com>
2026-05-27 17:37:36 +0200

    MT#61856 call_interfaces: block silence media, iteration bug
    For-cycle expects the `sink_ml` for setting the `call_media`
    at the upper level of the cycle, but instead the `monologue`
    is used, which is supposed to be used in the nested for-cycle.
    
    Looks like a copy-paste typo. Major fix.
    
    Change-Id: I225a51fde7fa8d10bac832ce5fc5444ee544e882
    (cherry picked from commit 3285aec607e0bdf093f03b83dd899a8453deb28f)

--- daemon/call_interfaces.c
+++ daemon/call_interfaces.c
@@ -3807,7 +3807,7 @@ static const char *call_block_silence_media(ng_command_ctx_t *ctx, bool on_off, 
 			/* now check if any sink ml media is susbcribed to any of monologue medias */
 			for (int i = 0; i < sink_ml->medias->len; i++)
 			{
-				struct call_media * sink_md = monologue->medias->pdata[i];
+				struct call_media * sink_md = sink_ml->medias->pdata[i];
 				if (!sink_md)
 					continue;
 				for (int j = 0; j < monologue->medias->len; j++)
