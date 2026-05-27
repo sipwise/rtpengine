7f77bf18a2b1a7816258e1e4ed0f74df1d372e76
Richard Fuchs <rfuchs@sipwise.com>
2026-05-27 08:59:55 -0400

    MT#55283 fix bencode buffer alignment
    Make sure data is 64-bit aligned. Fixes alignment issues on certain
    32-bit archs (armhf).
    
    Change-Id: I66c1fc20e97c62d3d5e266e874812aed74d295ed
    (cherry picked from commit 31375ca349c27289d1cc3fb9f78efa8ca99127cb)
    (cherry picked from commit 30fa1c61cb10c1fdb696632614ef840e27a3fd47)

--- lib/bencode.h
+++ lib/bencode.h
@@ -6,6 +6,8 @@
 
 #include "compat.h"
 
+#define BENCODE_ALLOC_ALIGN 8
+
 struct bencode_buffer;
 enum bencode_type;
 struct bencode_item;
@@ -34,7 +36,7 @@ struct bencode_item {
 	long long int value;	/* when decoding an integer, contains the value; otherwise used internally */
 	bencode_item_t *parent, *child, *last_child, *sibling;
 	bencode_buffer_t *buffer;
-	char __buf[0];
+	char __buf[0] __attribute__ ((aligned (BENCODE_ALLOC_ALIGN)));
 };
 
 struct bencode_buffer {
