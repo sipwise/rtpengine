287becb34b2511054fef9c31dd7bfe99e9677aeb
Richard Fuchs <rfuchs@sipwise.com>
2026-06-22 09:16:22 -0400

    MT#55283 more leniency for rejected streams
    Allow invalid payload types for rejected RTP streams.
    
    Allow empty codec store for zero-port streams.
    
    Has some side effects on tests.
    
    Closes #2122
    
    Change-Id: I28c63a3e5b7309084e7899a19ff4550e8f56aac5
    (cherry picked from commit 71cceeed5a25a8cc15872c86e76245d323b06a61)

--- t/auto-daemon-tests.pl
+++ t/auto-daemon-tests.pl
@@ -5846,7 +5846,7 @@ a=ssrc:1948313614 cname:Kk01/qU0PWi9Cacd
 a=content:main
 a=sendrecv
 a=rtcp:PORT
-m=application 0 RTP/AVP 0
+m=application 0 RTP/AVP 100
 c=IN IP4 0.0.0.0
 a=mid:5
 SDP
@@ -6144,7 +6144,7 @@ a=content:main
 a=sendrecv
 a=rtcp:PORT
 a=rtcp-mux
-m=application 0 RTP/AVP 0
+m=application 0 RTP/AVP 100
 c=IN IP4 0.0.0.0
 a=mid:5
 SDP
@@ -6200,7 +6200,7 @@ c=IN IP6 ::
 b=TIAS:5952000
 a=mid:2
 a=label:11
-m=application 0 RTP/SAVP 96
+m=application 0 RTP/SAVP 0
 c=IN IP4 0.0.0.0
 a=mid:5
 SDP
@@ -11889,7 +11889,7 @@ c=IN IP4 203.0.113.1
 a=rtpmap:8 PCMA/8000
 a=sendrecv
 a=rtcp:PORT
-m=audio 0 RTP/AVP 8
+m=audio 0 RTP/AVP 0
 c=IN IP4 0.0.0.0
 SDP
 # ^ technically fishy - rejected stream should not do offer/answer and should just
@@ -27513,7 +27513,7 @@ c=IN IP4 203.0.113.1
 a=rtpmap:8 PCMA/8000
 a=sendrecv
 a=rtcp:PORT
-m=video 0 RTP/AVP 0
+m=video 0 RTP/AVP 97
 c=IN IP4 0.0.0.0
 SDP
 
