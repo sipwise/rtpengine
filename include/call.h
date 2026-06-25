2cccbb6c627ca99cbc3e104ae9235fc8f510ce2d
Richard Fuchs <rfuchs@sipwise.com>
2026-06-25 08:02:58 -0400

    MT#65392 add "fast" flag for delete processing
    Change-Id: I16d4cdd9ecc8dbe612eac2ff002283b606f0f082
    (cherry picked from commit 6391c6bf9525aa296736cb3e0a5f43f349d52c09)
    (cherry picked from commit 1f5d1d4025a95193098ca8e0e5b09d3d10f3f040)

--- include/call.h
+++ include/call.h
@@ -914,7 +914,8 @@ void monologue_destroy(struct call_monologue *ml);
 int call_delete_branch_by_id(const str *callid, const str *branch,
 	const str *fromtag, const str *totag, ng_command_ctx_t *, int64_t delete_delay);
 int call_delete_branch(call_t *, const str *branch,
-	const str *fromtag, const str *totag, ng_command_ctx_t *, int64_t delete_delay);
+	const str *fromtag, const str *totag, ng_command_ctx_t *, int64_t delete_delay,
+	bool stats);
 void call_destroy(call_t *);
 struct call_media *call_media_new(call_t *call);
 void call_media_free(struct call_media **mdp);
