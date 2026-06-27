2e034c2ce4e0e89e9ae31652eb75c9fb53bd64a2
Alexander Bakker <ab@alexbakker.me>
2026-06-27 13:05:13 +0200

    MT#55283 Unref ports after re-subscribe
    Previously, the ports used for a monologue subscription would leak upon re-subscribe.
    ``__assign_stream_fds`` cleared the stream fd queue without decrementing the ref count. This
    prevented ``monologue_destroy`` from releasing the ports on unsubscribe.
    
    Closes #2125
    
    Change-Id: Ib86290d6a35e2388e34098f2847f1e34b3db2c5d
    (cherry picked from commit 250df2af5e1d56315c86339460438528afdb0102)

--- t/auto-daemon-tests-create.pl
+++ t/auto-daemon-tests-create.pl
@@ -381,7 +381,7 @@ rtpe_req('unsubscribe', 'extended connect w "all" bidirectional', {
 		flags => [],
 });
 
-snd($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
+snd_no($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
 rcv_no($sock_a);
 rcv_no($sock_b);
 rcv_no($sock_c);
@@ -766,7 +766,7 @@ rtpe_req('unsubscribe', 'extended connect w from-tags bidirectional', {
 		flags => [],
 });
 
-snd($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
+snd_no($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
 rcv_no($sock_a);
 rcv_no($sock_b);
 rcv_no($sock_c);
@@ -1144,7 +1144,7 @@ rtpe_req('unsubscribe', 'extended connect w from-tags', {
 		flags => [],
 });
 
-snd($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
+snd_no($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
 rcv_no($sock_a);
 rcv_no($sock_b);
 rcv_no($sock_c);
@@ -1703,7 +1703,7 @@ rtpe_req('unsubscribe', 'extended connect bidirectional', {
 		flags => [],
 });
 
-snd($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
+snd_no($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
 rcv_no($sock_a);
 rcv_no($sock_b);
 rcv_no($sock_c);
@@ -2249,7 +2249,7 @@ rtpe_req('unsubscribe', 'extended connect', {
 		flags => [],
 });
 
-snd($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
+snd_no($sock_a, $port_b, rtp (0, 1011, 4760, 0x1234, "\x11" x 160));
 rcv_no($sock_a);
 rcv_no($sock_b);
 rcv_no($sock_c);
