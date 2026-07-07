3139e19ef4276094504bffdd627facf181aa10d4
Richard Fuchs <rfuchs@sipwise.com>
2026-07-07 07:30:28 -0400

    MT#55283 fix compilation for kernel >= 7.1
    Change-Id: I004320a12ea77ec1e805394dd7d20ee62a73785c
    (cherry picked from commit 36a347ed7fe1dd255329e91b38cb91d595831fe3)

--- kernel-module/nft_rtpengine.c
+++ kernel-module/nft_rtpengine.c
@@ -6890,7 +6890,7 @@ static const struct nft_expr_ops rtpengine_inet_ops = {
 	.init			= rtpengine_expr_init,
 	.dump			= rtpengine_expr_dump,
 	.validate		= rtpengine_expr_validate,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0) && LINUX_VERSION_CODE < KERNEL_VERSION(7,1,0)
 	.reduce			= NFT_REDUCE_READONLY,
 #endif
 };
@@ -6902,7 +6902,7 @@ static const struct nft_expr_ops rtpengine_ipv4_ops = {
 	.init			= rtpengine_expr_init,
 	.dump			= rtpengine_expr_dump,
 	.validate		= rtpengine_expr_validate,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0) && LINUX_VERSION_CODE < KERNEL_VERSION(7,1,0)
 	.reduce			= NFT_REDUCE_READONLY,
 #endif
 };
@@ -6914,7 +6914,7 @@ static const struct nft_expr_ops rtpengine_ipv6_ops = {
 	.init			= rtpengine_expr_init,
 	.dump			= rtpengine_expr_dump,
 	.validate		= rtpengine_expr_validate,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0) && LINUX_VERSION_CODE < KERNEL_VERSION(7,1,0)
 	.reduce			= NFT_REDUCE_READONLY,
 #endif
 };
