e8ebc0d01d6a932f313cd28aa45d307dc84f7443
Richard Fuchs <rfuchs@sipwise.com>
2026-08-07 07:03:36 -0400

    MT#55283 update test for .reduce method
    6.12.101+ has removed this as well. Use the macro itself to determine
    whether this struct member exists.
    
    Change-Id: Ib43157567a78afc664398b9f22ee0a27b0facfa0
    (cherry picked from commit 83b05e9a11214f71e0e4d960c803171e96e7fcef)
    (cherry picked from commit 2dadd6e8d60a21546545f893ad8817031242ffba)

--- kernel-module/nft_rtpengine.c
+++ kernel-module/nft_rtpengine.c
@@ -6908,7 +6908,7 @@ static const struct nft_expr_ops rtpengine_inet_ops = {
 	.init			= rtpengine_expr_init,
 	.dump			= rtpengine_expr_dump,
 	.validate		= rtpengine_expr_validate,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0) && LINUX_VERSION_CODE < KERNEL_VERSION(7,1,0)
+#ifdef NFT_REDUCE_READONLY
 	.reduce			= NFT_REDUCE_READONLY,
 #endif
 };
@@ -6920,7 +6920,7 @@ static const struct nft_expr_ops rtpengine_ipv4_ops = {
 	.init			= rtpengine_expr_init,
 	.dump			= rtpengine_expr_dump,
 	.validate		= rtpengine_expr_validate,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0) && LINUX_VERSION_CODE < KERNEL_VERSION(7,1,0)
+#ifdef NFT_REDUCE_READONLY
 	.reduce			= NFT_REDUCE_READONLY,
 #endif
 };
@@ -6932,7 +6932,7 @@ static const struct nft_expr_ops rtpengine_ipv6_ops = {
 	.init			= rtpengine_expr_init,
 	.dump			= rtpengine_expr_dump,
 	.validate		= rtpengine_expr_validate,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,17,0) && LINUX_VERSION_CODE < KERNEL_VERSION(7,1,0)
+#ifdef NFT_REDUCE_READONLY
 	.reduce			= NFT_REDUCE_READONLY,
 #endif
 };
