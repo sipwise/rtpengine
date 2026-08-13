9e38ec348a9038929f5aae8df808616d0f70bb38
Richard Fuchs <rfuchs@sipwise.com>
2026-08-13 07:50:00 -0400

    MT#55283 fix compiler warnings
    Change-Id: Idd4162cbcdddead57b730f773289786b7b4cf3fe
    (cherry picked from commit fda6cbf8b153f508d78a03a7853f998c90a044c0)

--- daemon/call.c
+++ daemon/call.c
@@ -385,6 +385,7 @@ void kill_calls_timer(GSList *list, const char *url) {
 	GList *csl;
 	struct call_monologue *cm;
 	char *url_prefix = NULL, *url_suffix = NULL;
+	const char *needle;
 	struct xmlrpc_helper *xh = NULL;
 	char url_buf[128];
 
@@ -395,10 +396,10 @@ void kill_calls_timer(GSList *list, const char *url) {
 	if (url) {
 		xh = g_slice_alloc(sizeof(*xh));
 		url_prefix = NULL;
-		url_suffix = strstr(url, "%%");
-		if (url_suffix) {
-			url_prefix = strndup(url, url_suffix - url);
-			url_suffix = strdup(url_suffix + 2);
+		needle = strstr(url, "%%");
+		if (needle) {
+			url_prefix = strndup(url, needle - url);
+			url_suffix = strdup(needle + 2);
 		}
 		else
 			url_suffix = strdup(url);
